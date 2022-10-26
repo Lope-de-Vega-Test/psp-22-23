284986935ae3409251dfab276841e8b68bf16a1a
carlooslpez <100300979+carlooslpez@users.noreply.github.com>
2022-10-26 17:19:51 +0200

    Update ua1ex1.c

--- UA1/examen/dev_12/ua1ex1.c
+++ UA1/examen/dev_12/ua1ex1.c
@@ -7,16 +7,16 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdbool.h>
-int codigo; //variable para saber en que fork estamos en la funcion funcionfork
+int codigo;
 void funcionfork(){
     if(codigo==1){
-        printf("Soy el hijo 1, Mi padre es: %d, Mi PID es: %d\n",getppid(),getpid());  //Funcion para llamar al proceso hijo 1
+        printf("Soy el hijo 1, Mi padre es: %d, Mi PID es: %d\n",getppid(),getpid());
     }
     if(codigo==2){
-        printf("Soy el hijo 2, Mi padre es: %d, Mi PID es: %d\n",getppid(),getpid());  //Funcion para llamar al proceso hijo 2
+        printf("Soy el hijo 2, Mi padre es: %d, Mi PID es: %d\n",getppid(),getpid());
     }
     if(codigo==3){
-        printf("Soy el hijo 3, Mi padre es: %d, Mi PID es: %d\n",getppid(),getpid());  //Funcion para llamar al proceso hijo 3
+        printf("Soy el hijo 3, Mi padre es: %d, Mi PID es: %d\n",getppid(),getpid());
 
     }
     
@@ -33,9 +33,9 @@ void main(){
   if(pid_hijo1==0){ //Crea el Hijo 1
     codigo=1;
     funcionfork();
-    sleep(2);
     exit(0);
   }
+  pid_hijo1 = wait(NULL); //espera la finalización del proceso hijo 1
   pid_hijo2=fork();
   if (pid_hijo2 == -1 ) //Ha ocurrido un error 
   {
@@ -45,9 +45,9 @@ void main(){
   if(pid_hijo2==0){ //Crea el Hijo 2
   codigo=2;
     funcionfork();
-    sleep(2);
     exit(0);
   }
+pid_hijo2 = wait(NULL); //espera la finalización del proceso hijo 2
   pid_hijo3=fork();
   if (pid_hijo3 == -1 ) //Ha ocurrido un error 
   {
@@ -57,14 +57,12 @@ void main(){
   if(pid_hijo3==0){ //Crea el Hijo 3
     codigo=3;
     funcionfork();
-    sleep(2);
     exit(0);
   }
   else{
       //Nos encontramos en Proceso Padre
   }
-  pid_hijo1 = wait(NULL); //espera la finalización del proceso hijo 1
-  pid_hijo2 = wait(NULL); //espera la finalización del proceso hijo 2
   pid_hijo3 = wait(NULL); //espera la finalización del proceso hijo 3
   printf("Soy el Proceso PADRE, mi PID es: %d\n",getpid());
   exit(0);
+}
