abe83f228abf1ab4732b08c3c63748e25a07be93
Rafael Damian Cristea <114081192+MrDamianCR@users.noreply.github.com>
2022-10-26 17:23:26 +0200

    Create ua1ex1.c

---
+++ UA1/examen/dev11/ua1ex1.c
@@ -0,0 +1,41 @@
+//Rafael Damian Cristea
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+//Funcion para mostrar los datos, usando getpid() y getppid()
+void fMostrar(){
+    printf("Soy el proceso hijo \n\t Mi PID es %d, El PID de mi padre es: %d.\n",  getpid(), getppid() );
+}
+
+int main()
+{
+
+for (int i = 0; i < 3; i++) {
+    
+    pid_t pid = fork();
+    
+    if(pid == -1){
+        //En caso de que nose pueda crear los procesos hijo saltara un error
+        printf("No se han podido crear los procesos hijo...");
+        exit(-1);
+    }
+    else if (pid == 0){
+        //Proceso hijo
+        fMostrar(); //Usamos la funcion anteriormente declarada
+        exit(EXIT_SUCCESS); //En caso de que haya funcionado, se sale del if para evitar la creacion de más hijos
+    }
+    else{
+        //Proceso padre
+    }
+    
+}
+
+for (int i = 0; i < 3; i++) {
+    int status;
+    pid_t pid = wait(&status); // Wait espera a que los hijos terminen de ejecutarse
+}
+
+printf("\nEl PID del PADRE DE TODOS es: %d",  getpid());
+}
