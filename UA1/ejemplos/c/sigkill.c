54143d2e81d474747e0b5314f0593e2e23a15e4a
carlooslpez <100300979+carlooslpez@users.noreply.github.com>
2022-10-26 16:51:41 +0200

    Update sigkill.c

--- UA1/ejemplos/c/sigkill.c
+++ UA1/ejemplos/c/sigkill.c
@@ -7,73 +7,47 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdbool.h>
-
-int contador_padre = 0;
-bool end = false;
-
-// Gestion de senyales en proceso padre       
-void gestion_padre (int segnal)
-{
-    contador_padre++;    
-    switch(segnal)
-    {
-        case SIGUSR1:
-            printf ("\t...Padre recibe senyal SIGUSR1 (%d) (contador padre: %d)", segnal, contador_padre);
-            break;
-          
-        case SIGTERM:
-            printf ("\t...Padre recibe senyal SIGTERM (%d) (contador padre: %d)", segnal, contador_padre);
-            printf ("\n\n\t\t\tOh no! It's TIME TO DIE!\n\n");            
-            end=true;
-            break;
-            
-        default: // JIC
-            printf ("\tPadre recibe senyal ... %d (contador padre: %d) \n", segnal, contador_padre);
-            break;
-    }
+void main(){
+  pid_t pid_hijo1,pid_hijo2,pid_hijo3;
+  pid_hijo1=fork();
+  if (pid_hijo1 == -1 ) //Ha ocurrido un error 
+  {
+    printf("No se ha podido crear el proceso hijo 1...");
+    exit(-1);       
+  }
+  if(pid_hijo1==0){ //Crea el Hijo 1
+    printf("Soy el hijo 1, Mi padre es: %d, Mi PID es: %d\n",getppid(),getpid());
+    sleep(2);
+    exit(0);
+  }
+  pid_hijo2=fork();
+  if (pid_hijo2 == -1 ) //Ha ocurrido un error 
+  {
+    printf("No se ha podido crear el proceso hijo 2...");
+    exit(-1);       
+  }
+  if(pid_hijo2==0){ //Crea el Hijo 2
+    printf("Soy el hijo 2, Mi padre es: %d, Mi PID es: %d\n",getppid(),getpid());
+    sleep(2);
+    exit(0);
+  }
+  pid_hijo3=fork();
+  if (pid_hijo3 == -1 ) //Ha ocurrido un error 
+  {
+    printf("No se ha podido crear el proceso hijo 3...");
+    exit(-1);       
+  }
+  if(pid_hijo3==0){ //Crea el Hijo 3
+    printf("Soy el hijo 3, Mi padre es: %d, Mi PID es: %d\n",getppid(),getpid());
+    sleep(2);
+    exit(0);
+  }
+  else{
+      //Nos encontramos en Proceso Padre
+  }
+  pid_hijo1 = wait(NULL); //espera la finalización del proceso hijo 1
+  pid_hijo2 = wait(NULL); //espera la finalización del proceso hijo 2
+  pid_hijo3 = wait(NULL); //espera la finalización del proceso hijo 3
+  printf("Soy el Proceso PADRE, mi PID es: %d\n",getpid());
+  exit(0);
 }
-
-
-int main ()
-{
-    int iterations = 3;
-    int pid_padre, pid_hijo;
-
-    pid_padre = getpid ();
-    pid_hijo = fork ();		// Creamos el Proceso Hijo
-
-    switch (pid_hijo)
-    {
-        case -1:
-          printf ("Error al crear el proceso hijo...\n");
-          exit (-1);
-          
-        case 0:			// Proceso Hijo
-            for(int i=0; i<iterations; i++)
-            {
-                sleep(2);
-                kill (pid_padre, SIGUSR1);	// Proceso Hijo envía senyal SIGUSR1 al Proceso Padre
-            }
-            printf ("\n\t\t\tFather ... Your time has reached to the end ...\n\n");
-            kill (pid_padre, SIGTERM);	// Proceso Hijo envía senyal SIGKILL al Proceso Padre
-            sleep(2);
-            break;
-            
-        default:			// Proceso Padre 
-            // tratamiento de la senyal en proceso padre
-	    signal (SIGUSR1, gestion_padre);
-            signal (SIGTERM, gestion_padre);
-            
-	    // Bucle Infinito hasta recibir SIGTERM ...	
-            while (!end)
-            {
-                printf ("\nProceso Padre esperando senyal ... ");
-                pause();		// Proceso Padre espera hasta recibir una senyal del hijo
-            }
-            printf ("\t\t\t ... I am going to die ...\n\n");
-            break;
-    }
-    
-    return 0;
-}
-
