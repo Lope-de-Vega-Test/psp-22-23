fd8e86bc10326d9fd0d299d45319e1d208fe8faa
Adrian Luque <ladriank.gaming@gmail.com>
2022-10-26 03:12:44 +0200

    Deletion

--- UA1/ejemplos/c/ejemploExec.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-void main()
-{
-       printf("Ejemplo de uso de exec():");      
-       printf("Los archivos en el directorio son:\n");
-       execl("/bin/ls", "ls", "-l",(char *)NULL);
-       printf("¡¡¡ Esto no se ejecuta !!!\n");
-}
