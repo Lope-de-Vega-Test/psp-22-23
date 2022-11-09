d9787b9756981f90c4df20c2db17604a8d0e8992
lucialunaposadas <114489539+lucialunaposadas@users.noreply.github.com>
2022-11-09 15:37:08 +0100

    Update ua2tarea1fr1.java

--- UA2/tareas/dev_20/tarea_1/ua2tarea1fr1.java
+++ UA2/tareas/dev_20/tarea_1/ua2tarea1fr1.java
@@ -1,65 +1,12 @@
 import java.util.Scanner;
 
-
-
-
-
-
-
-
-
-
 /**
-
-
-
 
 *
-
-
-
-
 * @author Lucía Luna
-
-
-
-
 */
-
-
-
-
-
-
-
-
-
 
 public class BloquesNoSincronizados {
-
-
-
-
-
-
-
-
-
-
-/**
-
-
-
-
-* @param args the command line arguments
-
-
-
-
-*/
-
-
-
 
 public static void main(String[] args) {
 
@@ -77,22 +24,6 @@ System.out.println("Hilos: Bloques NO Sincronizados");
 
 
 System.out.println("-------------------------------");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -134,18 +65,6 @@ HiloSumador hilo5 = new HiloSumador("Soy el hilo 5", cont);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 System.out.println("Comienza la ejecución del hilo 1 ...");
 
 
@@ -158,17 +77,7 @@ System.out.println("--------------------------------------");
 
 //comienza hilo 1
 
-
-
-
 hilo1.start();
-
-
-
-
-
-
-
 
 
 
@@ -184,15 +93,7 @@ System.out.println("--------------------------------------");
 
 //comienza hilo 2
 
-
-
-
 hilo2.start();
-
-
-
-
-
 
 
 
@@ -206,21 +107,9 @@ System.out.println("Comienza la ejecución del hilo 3 ...");
 System.out.println("--------------------------------------");
 
 
-
-
 //comienza hilo 3
 
-
-
-
 hilo3.start();
-
-
-
-
-
-
-
 
 
 
@@ -232,21 +121,9 @@ System.out.println("Comienza la ejecución del hilo 4 ...");
 System.out.println("--------------------------------------");
 
 
-
-
 //comienza hilo 4
 
-
-
-
 hilo4.start();
-
-
-
-
-
-
-
 
 
 
@@ -257,113 +134,37 @@ System.out.println("Comienza la ejecución del hilo 5 ...");
 
 System.out.println("--------------------------------------");
 
-
-
-
 //comienza hilo 5
-
-
-
 
 hilo5.start();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 try {
-
-
-
 
 //join() suele utilizarse para mantener un orden en la secuencia de los hilos
 
-
-
-
 hilo1.join();
-
-
-
 
 hilo2.join();
 
-
-
-
 hilo3.join();
 
-
-
-
 hilo4.join();
-
-
-
 
 hilo5.join();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
 
 
 
 catch (InterruptedException e)
-
-
-
-
 {
-
-
-
 
 // Nothing to do here ...
 
-
-
-
 }
-
-
-
-
-
-
-
-
 
 
 //Mostramos la suma global
@@ -380,387 +181,112 @@ System.out.println("... Finaliza la ejecución de todos los hilos ");
 
 
 
-
 System.out.println("--------------------------------------");
-
-
 
 
 System.out.println("Valor Final del Contador: " + cont.valor());
 
 
-
-
 System.out.println("--------------------------------------");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //Clase HiloSumador
 
 
-
-
-
-
-
-
-
-
 package bloquesnosincronizados;
 
-
-
-
 /**
-
-
-
-
 *
-
-
-
-
 * @author Lucía Luna
-
-
-
-
 */
 
 
-
-
 class HiloSumador extends Thread
-
-
-
-
 {
 
-
-
-
 private Contador contador;
-
-
-
-
-
-
-
-
 
 
 public HiloSumador(String nombre, Contador c) {
 
 
-
-
 setName(nombre);
-
-
-
 
 contador = c;
 
-
-
-
 }
-
-
-
-
-
-
-
 
 
 
 public void run() {
 
-
-
-
 for(int j=0; j<1000;j++)
-
-
-
 
 {
 
 
-
-
 contador.incrementa();
 
-
-
-
-
-
-
-
-
-
 }
-
-
-
 
 System.out.println(getName() + " - contador vale " + contador.valor());
 
-
-
-
 }
-
-
 
 
 } // Fin Class HiloSumador
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //Clase Contador
-
-
-
-
 package bloquesnosincronizados;
-
-
-
-
 /**
-
-
-
-
 *
-
-
-
-
 * @author Lucía Luna
-
-
-
-
 */
 
-
-
-
 class Contador
-
-
-
-
 {
-
-
-
-
 private int c = 0; // Atributo Contador
 
+  Contador (int c) {
 
-
-
-Contador (int c) {
-
-
-
-
-this.c = c;
-
-
-
+  this.c = c;
 
 }
-
-
-
-
-
-
-
 
 
 
 public void incrementa() {
 
-
-
-
 c++;
 
-
-
-
 }
-
-
-
-
-
-
-
 
 
 
 public void decrementa() {
 
-
-
-
 c--;
 
-
-
-
 }
-
-
-
-
-
-
-
 
 
 
 public int valor() {
 
-
-
-
-return c;
-
-
-
+  return c;
 
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-package bloquesnosincronizados;
+
 
 
 
