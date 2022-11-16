1d9b7c8ddd45489112db28a0c867acaad0480100
JavCast03 <101286156+JavCast03@users.noreply.github.com>
2022-11-16 17:10:04 +0100

    Update act2ejecutable.java

--- UA1/tareas/dev_9/act2/act2ejecutable.java
+++ UA1/tareas/dev_9/act2/act2ejecutable.java
@@ -7,7 +7,7 @@ public class act2ejecutable {
     
 public static void main(String[] args) {
         File directorio=new File("build\\classes\\");
-        ProcessBuilder pb = new ProcessBuilder("java", "Actividad2PSPEjecutable");
+        ProcessBuilder pb = new ProcessBuilder("java", "tarea2_1");
                     pb.directory(directorio);	
 		try {
 			
