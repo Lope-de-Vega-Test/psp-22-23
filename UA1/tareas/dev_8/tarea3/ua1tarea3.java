0d1726553e8508e853ec146e484d02c373b96f5e
front440 <fjcamposgutierrez@gmail.com>
2022-11-16 16:39:37 +0100

    añadiendo tarea 3 de la ua1

---
+++ UA1/tareas/dev_8/tarea3/ua1tarea3.java
@@ -0,0 +1,14 @@
+package dev_8.tarea3;
+
+public class ua1tarea3 {
+    
+    public static void main(String[] args) {
+
+        switch (args.length) {
+            case 0:
+                System.exit(0);
+                break;
+            
+        }
+    }
+}
