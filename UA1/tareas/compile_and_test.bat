06e743d65dcf4849ffc552f6792d893febb0c13d
JRC32 <101286025+JRC32@users.noreply.github.com>
2022-11-17 13:06:24 +0100

    Delete compile_and_test.bat

--- UA1/tareas/compile_and_test.bat
+++
@@ -1,7 +0,0 @@
-cls
-del *.class
-javac -cp . Tarea1.java
-
-javac -cp .;..\..\bin\junit-jupiter-5.8.2.jar;..\..\bin\junit-jupiter-api-5.8.2.jar;..\..\bin\apiguardian-api-1.1.2.jar Tarea1Test.java
-
-java -cp .;..\..\bin\junit-jupiter-5.8.2.jar;..\..\bin\junit-jupiter-api-5.8.2.jar;..\..\bin\apiguardian-api-1.1.2.jar Tarea1Test
