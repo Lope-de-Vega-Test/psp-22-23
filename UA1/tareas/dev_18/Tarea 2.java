f333d6c103473973d95b4ee1b77521cd644c3ecf
Guillermo <56632130+guillejito@users.noreply.github.com>
2022-11-17 15:16:15 +0100

    Update Tarea 2.java

--- UA1/tareas/dev_18/Tarea 2.java
+++ UA1/tareas/dev_18/Tarea 2.java
@@ -1,43 +1,39 @@
+package tarea2;
+
 import java.io.File;
 import java.io.IOException;
 
 public class Tarea2 {
-    
+
     public static void main(String[] args) {
-		
-		
-		File directorio = new File("build\\classes\\");
-		
-		ProcessBuilder pb = new ProcessBuilder("java", "Lanzar");	// Estudiar como hacerlo con paquetes: com.ceslopedevega....
-		pb.directory(directorio);
-				
-		try {
-		
-			pb.redirectInput(ProcessBuilder.Redirect.INHERIT);
-			pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-						
-			Process p = pb.start();
-			
-			try {
-      
-				p.waitFor();
-        
-			} 
-      
-      catch (InterruptedException e) {
-      
-				e.printStackTrace();
-        
-			}
-      
-		} 
-    
-    catch (IOException e1) {
-			
-			e1.printStackTrace();
-      
-		}
-    
-	}
-    
+
+        File directorio = new File("build\\classes\\");
+
+        ProcessBuilder pb = new ProcessBuilder("java", "Lanzar");
+        pb.directory(directorio);
+
+        try {
+
+            pb.redirectInput(ProcessBuilder.Redirect.INHERIT);
+            pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+
+            Process p = pb.start();
+
+            try {
+
+                p.waitFor();
+
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+
+            }
+
+        } catch (IOException e1) {
+
+            e1.printStackTrace();
+        }
+
+    }
+
 }
