ab1e2241eba89d9a3b55cfce0a0906bc37f42e17
lucialunaposadas <114489539+lucialunaposadas@users.noreply.github.com>
2022-11-20 10:54:34 +0100

    Update ua2tareafr2.java

--- UA2/tareas/dev_20/tarea_1/ua2tareafr2.java
+++ UA2/tareas/dev_20/tarea_1/ua2tareafr2.java
@@ -1,246 +1,5 @@
-package bloquesnosincronizados;
+package u2tareafr2.java;
 
-import java.util.Scanner;
-
-/**
-*
-* @author Lucía Luna
-*/
-
-public class BloquesNoSincronizados {
-
-public static void main(String[] args) {
-
-
-
-
-System.out.println("-------------------------------");
-
-
-
-
-System.out.println("Hilos: Bloques NO Sincronizados");
-
-
-
-
-System.out.println("-------------------------------");
-
-
-
-
-
-
-Contador cont = new Contador(1000);
-
-
-
-
-//Creamos los 5 hilos
-
-
-
-
-HiloSumador hilo1 = new HiloSumador("Soy el hilo 1", cont);
-
-
-
-
-HiloSumador hilo2 = new HiloSumador("Soy el hilo 2", cont);
-
-
-
-
-HiloSumador hilo3 = new HiloSumador("Soy el hilo 3", cont);
-
-
-
-
-HiloSumador hilo4 = new HiloSumador("Soy el hilo 4", cont);
-
-
-
-
-HiloSumador hilo5 = new HiloSumador("Soy el hilo 5", cont);
-
-
-
-
-System.out.println("Comienza la ejecución del hilo 1 ...");
-
-
-
-
-System.out.println("--------------------------------------");
-
-
-
-
-//comienza hilo 1
-
-hilo1.start();
-
-
-
-System.out.println("Comienza la ejecución del hilo 2 ...");
-
-
-
-
-System.out.println("--------------------------------------");
-
-
-
-
-//comienza hilo 2
-
-hilo2.start();
-
-
-
-
-
-System.out.println("Comienza la ejecución del hilo 3 ...");
-
-
-
-
-System.out.println("--------------------------------------");
-
-
-//comienza hilo 3
-
-hilo3.start();
-
-
-
-System.out.println("Comienza la ejecución del hilo 4 ...");
-
-
-
-
-System.out.println("--------------------------------------");
-
-
-//comienza hilo 4
-
-hilo4.start();
-
-
-
-System.out.println("Comienza la ejecución del hilo 5 ...");
-
-
-
-
-System.out.println("--------------------------------------");
-
-//comienza hilo 5
-
-hilo5.start();
-
-
-
-try {
-
-//join() suele utilizarse para mantener un orden en la secuencia de los hilos
-
-hilo1.join();
-
-hilo2.join();
-
-hilo3.join();
-
-hilo4.join();
-
-hilo5.join();
-
-
-}
-
-
-
-catch (InterruptedException e)
-{
-
-// Nothing to do here ...
-
-}
-
-
-//Mostramos la suma global
-
-
-
-
-System.out.println("--------------------------------------");
-
-
-
-
-System.out.println("... Finaliza la ejecución de todos los hilos ");
-
-
-
-System.out.println("--------------------------------------");
-
-
-System.out.println("Valor Final del Contador: " + cont.valor());
-
-
-System.out.println("--------------------------------------");
-}
-
-
-
-}
-
-//HiloSumador
-package bloquesnosincronizados;
-
-/**
-*
-* @author Lucía Luna
-*/
-
-
-class HiloSumador extends Thread
-{
-
-private Contador contador;
-
-
-public HiloSumador(String nombre, Contador c) {
-
-
-setName(nombre);
-
-contador = c;
-
-}
-
-
-
-public void run() {
-
-for(int j=0; j<1000;j++)
-
-{
-
-
-contador.incrementa();
-
-}
-
-System.out.println(getName() + " - contador vale " + contador.valor());
-
-}
-
-
-} // Fin Class HiloSumador
-    
-//Contador
-package bloquesnosincronizados;
 /**
  *
  * @author Lucía Luna
@@ -252,17 +11,122 @@ class Contador
         this.c = c;
     }
 
-    public void incrementa() {
+    public synchronized void incrementa() {
         c++;
     }
 
-    public void decrementa() {
-        c--;
-    }
+    
 
     public int valor() {
         return c;
     }
+} // Final Class Contador
+
+  
+
+class ExecutionTimer
+{
+    private long startTime = 0;
+    private long endTime = 0;
+    private long timeElapsed = 0;
+        
+    public void start() {
+        startTime = System.nanoTime();
+    }
     
-    
+    public void stop() {
+        endTime = System.nanoTime();
+    }
+
+    public void elapsedTime() {
+        timeElapsed = endTime - startTime;
+    }
+
+    public void printElapsedTime()    
+    {
+        elapsedTime();
+        System.out.println("Execution time in nanoseconds: " + timeElapsed);
+        System.out.println("Execution time in milliseconds: " + timeElapsed / 1000000);
+    }
+}
+
+
+
+class hilo extends Thread 
+{
+    private Contador contador;
+
+    public hilo(String nombre, Contador c) {
+        setName(nombre);
+        contador = c;
+    }
+
+   
+    public void run() {
+        
+        //synchronized(contador)
+        {
+        
+          for(int j=0; j<1000;j++) // TODO: Cambia a 3000 en ambos a ver qué ocurre ...
+        {
+            contador.incrementa();
+         
+        }
+        
+        }
+      
+        System.out.println(getName() + " - contador vale " + contador.valor());
+    }
+
+   
+ // Final Class HiloSumador
+
+public class ua2tarea1fr2 {
+
+     public static void main(String[] args) {
+        
+        System.out.println("-------------------------------");
+        System.out.println("Hilos: Tarea 1 con funcion incrementar sincronizada");
+        System.out.println("-------------------------------");
+        ExecutionTimer timer = new ExecutionTimer();
+
+        Contador cont = new Contador(0);
+        hilo hilo1 = new hilo("Hilo 1", cont);
+        hilo hilo2 = new hilo("Hilo 2", cont);
+        hilo hilo3 = new hilo("Hilo 3", cont);
+        hilo hilo4 = new hilo("Hilo 4", cont);
+        hilo hilo5 = new hilo("Hilo 5", cont);
+
+        timer.start();
+        System.out.println("Comienza la ejecución de los hilos ...");
+        System.out.println("--------------------------------------");
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
+        hilo4.start();
+        hilo5.start();
+
+        try {
+            hilo1.join();
+            hilo2.join();
+            hilo3.join();
+            hilo4.join();
+            hilo5.join();
+            
+        }
+        catch (InterruptedException e)
+        {
+            // Nothing to do here ...
+        }
+
+        System.out.println("--------------------------------------");
+        System.out.println("... Finaliza la ejecución de los hilos");
+        System.out.println("--------------------------------------");
+        System.out.println("Valor Final del Contador: " + cont.valor());
+        System.out.println("--------------------------------------");
+        
+        timer.stop();
+        timer.printElapsedTime();
+    }
+}
 }
