47fba4a481c14b1446b7b4edfff4d985bdae5844
DaniBalsera <99357708+DaniBalsera@users.noreply.github.com>
2022-12-11 11:09:55 +0100

    Create PSP_UA3_Tarea2Cliente.java

---
+++ UA3/tareas/dev_14/Tarea2/PSP_UA3_Tarea2Cliente.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.net.*;
+/*
+ * 
+ * Autor: @Daniel Fernández Balsera
+ * 
+ */
+public class PSP_UA3_Tarea2Cliente {
+    public static void main(String[] arg) throws IOException {
+
+
+ 
+        String Host = "localhost";
+          int Puerto = 6000;// Puerto
+    
+        Socket Cliente = new Socket(Host, Puerto);
+        InetAddress i = Cliente.getInetAddress();
+/*
+ * 
+ * 
+ *  Aquí mostramos los datos del cliente
+ * 
+ *  Puerto Local -> Obtiene el puerto local del cliente
+ *  Puerto remoto -> Obtiene el puerto remoto
+ *  Nombre Host/IP -> Obtiene en este caso la dirección IP
+ *  Host Remoto -> Obtiene el host remoto
+ *  IP -> Obtiene la IP del host remoto
+ * 
+ */
+
+        System.out.println("Puerto Local: " + Cliente.getLocalPort());
+        System.out.println("Puerto Remoto: " + Cliente.getPort());
+        System.out.println("Nombre Host/IP: " + Cliente.getInetAddress());
+        System.out.println("Host Remoto: " + i.getHostName().toString());
+        System.out.println("IP Host Remoto: " + i.getHostAddress().toString());
+    
+        Cliente.close();
+      }
+}
