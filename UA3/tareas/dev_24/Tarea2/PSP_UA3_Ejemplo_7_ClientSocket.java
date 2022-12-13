d1cbe92c51dc553a77ef4e0a8f9880ea373e5aec
PainnZ <101286189+PainnZ@users.noreply.github.com>
2022-12-13 15:54:53 +0100

    Create PSP_UA3_Ejemplo_7_ClientSocket.java

---
+++ UA3/tareas/dev_24/Tarea2/PSP_UA3_Ejemplo_7_ClientSocket.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+
+public class PSP_UA3_Ejemplo_7_ClientSocket {
+  public static void main(String[] arg) throws IOException {
+    String Host = "localhost";
+	  int Puerto = 6000;// Puerto
+
+    Socket Cliente = new Socket(Host, Puerto);
+    InetAddress i = Cliente.getInetAddress();
+    System.out.println("Puerto Local: " + Cliente.getLocalPort());
+    System.out.println("Puerto Remoto: " + Cliente.getPort());
+    System.out.println("Nombre Host/IP: " + Cliente.getInetAddress());
+    System.out.println("Host Remoto: " + i.getHostName().toString());
+    System.out.println("IP Host Remoto: " + i.getHostAddress().toString());
+
+    Cliente.close();
+  }
+
+}
