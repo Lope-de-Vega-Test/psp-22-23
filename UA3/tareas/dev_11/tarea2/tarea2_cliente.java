fcb9309a3fdd9a4fc9712c500f097fc84ba05698
Rafael Damian Cristea <114081192+MrDamianCR@users.noreply.github.com>
2022-12-13 18:08:35 +0100

    Create tarea2_cliente.java

---
+++ UA3/tareas/dev_11/tarea2/tarea2_cliente.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+
+public class tarea2_cliente {
+  public static void main(String[] arg) throws IOException {
+    String Host = "localhost";
+	  int Puerto = 6000;// Puerto
+
+    Socket Cliente = new Socket(Host, Puerto);
+    InetAddress i = Cliente.getInetAddress();
+    System.out.println("Puerto Local: " + Cliente.getLocalPort());
+    System.out.println("Puerto Remoto: " + Cliente.getPort());
+    System.out.println("Nombre Host/IP: " + Cliente.getInetAddress());
+    System.out.println("Host Remoto: " + i.getHostName().toString());
+    System.out.println("IP Host Remoto: " + i.getHostAddress().toString());
+
+    Cliente.close();
+  }
+
+}
