5c0bf241648889781cc61cb86b5bbe1169d96e29
daniel@onimagin.com <daniel@onimagin.com>
2022-12-13 22:50:38 +0100

    Enunciado Actividad Evaluiable UA3 disponible

---
+++ UA3/actividad/team_0/MiembroToken.java
@@ -0,0 +1,37 @@
+/**
+ * @file        MiembroToken.java
+ *
+ * @brief       Solución para la actividad evaluable de la UA3.
+ * @date        14/12/2022
+ * @author      Nombre Apellidos Miembro 1 - github@email (dev_A)
+ * @author      Nombre Apellidos Miembro 2 - github@email (dev_B)
+ * @author      Nombre Apellidos Miembro 3 - github@email (dev_C)
+ * @author      Nombre Apellidos Miembro 4 - github@email (dev_D)
+ *
+ * @note        ¡Pero qué diablos!
+ * @note        https://docs.google.com/presentation/d/e/2PACX-1vQzxbSl2IXLxelLggksAWEQd2tDT-5sNqETQCpIHCnNBqjoSmbvlJdCcOfu_rjQLz_BN6lsoOjimqSO/pub?start=false&loop=false&delayms=3000
+ */
+
+/*
+* @class    MiembroToken
+* @brief    Clase para simular el comportamiento de una red token ring
+*
+* @todo FR1 [5 puntos]: Implementa la clase MiembroToken con la funcionalidad descrita previamente. Para comprobar la correcta ejecución del sistema, la clase MiembroToken debe imprimir por pantalla la suficiente información para ver el estado de cada Miembro de la red.
+* @todo FR2 [2,5 puntos]: Mejora la clase MiembroToken para poder ejecutar su funcionalidad, en este caso, dormir una cantidad de tiempo determinada, como un hilo.
+* @todo FR4 [1 punto]: Mejora la clase MiembroToken para crear una red token ring de anillo doble, es decir, se puede tener otro token en otro anillo virtual, en sentido contrario.
+*/
+
+public class MiembroToken
+{
+    /* @brief       Classic java main, starting execution
+     * @param       arg[0] = [id]
+     * @param       arg[1] = [puerto]
+     * @param       arg[2] = [token_al_inicio]
+     * @param       arg[3] = [soy_el_ultimo]
+     */
+    public static void main(String[] args)
+    {
+
+    } // End of main()
+
+} // End of class
