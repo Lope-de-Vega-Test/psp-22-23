51a6488f7c47f01ff999d5f1f93a4907fee800a4
PainnZ <101286189+PainnZ@users.noreply.github.com>
2022-12-13 15:54:22 +0100

    Create PSP_UA3_Ejemplo_7_ServerSocket.java

---
+++ UA3/tareas/dev_24/Tarea2/PSP_UA3_Ejemplo_7_ServerSocket.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.net.*;
+
+public class PSP_UA3_Ejemplo_7_ServerSocket {
+  public static void main(String[] arg) throws IOException {
+    
+	  int Puerto = 6000;// Puerto
+    int backlog = 50;//Backlog
+    InetAddress bindAddr = InetAddress.getByName("10.2.1.151");
+    
+	ServerSocket Servidor = new ServerSocket( Puerto , backlog , bindAddr );
+    System.out.println("Escuchando en " + Servidor.getLocalPort());
+
+    Socket cliente1 = Servidor.accept();
+    // Realizar Acciones con cliente 1
+    System.out.println("Escuchando en AAAAAAAAAAAAA" + Servidor.getLocalPort());
+    Socket cliente2 = Servidor.accept();
+    // Realizar Acciones con cliente 2
+
+    Servidor.close();
+  }
+
+}
