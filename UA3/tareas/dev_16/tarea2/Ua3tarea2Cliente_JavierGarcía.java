e58f7d91a02ce737185c7f4a1a4e6b1c1d166190
JavierGarciaRuiz <99357715+JavierGarciaRuiz@users.noreply.github.com>
2022-12-14 12:37:39 +0100

    Update Ua3tarea2Cliente_JavierGarcía.java

--- UA3/tareas/dev_16/tarea2/Ua3tarea2Cliente_JavierGarcía.java
+++ UA3/tareas/dev_16/tarea2/Ua3tarea2Cliente_JavierGarcía.java
@@ -10,17 +10,39 @@ public class Ua3tarea2Cliente_JavierGarcía {
         // TODO code application logic here
         
         String Host = "localhost";
-        int Puerto = 7000;// Puerto
+	int Puerto = 7000;//puerto remoto
+
+	System.out.println("PROGRAMA CLIENTE INICIADO....");
+	Socket Cliente = new Socket(Host, Puerto);
+
+	// CREO FLUJO DE SALIDA AL SERVIDOR
+	DataOutputStream flujoSalida = new DataOutputStream(Cliente.getOutputStream());
+
+	// ENVIO UN SALUDO AL SERVIDOR
+	flujoSalida.writeUTF("Saludos al SERVIDOR DESDE EL CLIENTE");
+
+	// CREO FLUJO DE ENTRADA AL SERVIDOR
+	DataInputStream flujoEntrada = new DataInputStream(Cliente.getInputStream());
+
+	// EL SERVIDOR ME ENVIA UN MENSAJE
+	System.out.println("Recibiendo del SERVIDOR: \n\t" + flujoEntrada.readUTF());
         
-    Socket Cliente1 = new Socket(Host, Puerto);
-    InetAddress i = Cliente1.getInetAddress();
-    System.out.println("Puerto Local: " + Cliente1.getLocalPort());
-    System.out.println("Puerto Remoto: " + Cliente1.getPort());
-    System.out.println("Nombre Host/IP: " + Cliente1.getInetAddress());
-    System.out.println("Host Remoto: " + i.getHostName().toString());
-    System.out.println("IP Host Remoto: " + i.getHostAddress().toString());
+        
+        // INFORMACIÓN DEL CLIENTE  
+        InetAddress i = Cliente.getInetAddress();
+        System.out.println("Puerto Local: " + Cliente.getLocalPort());
+        System.out.println("Puerto Remoto: " + Cliente.getPort());
+        System.out.println("Nombre Host/IP: " + Cliente.getInetAddress());
+        
+        
+	// CERRAR STREAMS Y SOCKETS
+	flujoEntrada.close();
+	flujoSalida.close();
+	Cliente.close();
     
-    Cliente1.close();   
+    
+    
+    
     }
     
 }
