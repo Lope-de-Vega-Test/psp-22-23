f69f17f8c00fff4efeb3c5559efd6c16bd40c048
josepa10 <91134279+jopajapo10@users.noreply.github.com>
2022-12-17 19:00:06 +0100

    Tarea equipo
    Hecho por Jose Pablo Jaraba, Carlos Lopez, Francisco Campos y Jose Ramón

---
+++ UA3/tareas/dev_17/team_17/Lanzador.java
@@ -0,0 +1,20 @@
+
+//execute:
+//java Lanzador.java
+//importante encontrarse en la carpeta del archivo Lanzador.java a la hora de ejecutar y que ambos
+//archivos esten en la misma carpeta
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Lanzador {
+
+	public static void main(String[] args) throws IOException {
+        String dir = System.getProperty("user.dir");
+        Runtime.getRuntime().exec("cmd.exe /c start cmd /k java MiembroToken.java 3 10002 0 1");
+        Runtime.getRuntime().exec("cmd.exe /c start cmd /k java MiembroToken.java 2 10001 0 0");
+        Runtime.getRuntime().exec("cmd.exe /c start cmd /k java MiembroToken.java 1 10000 1 0");
+
+
+    }
+}
