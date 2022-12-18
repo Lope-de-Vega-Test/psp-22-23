98c20bb14efa61d40338e58053928b6b6b8faa5c
PainnZ <101286189+PainnZ@users.noreply.github.com>
2022-12-18 20:13:37 +0100

    Update ua2tarea2.java

--- UA2/Tareas/dev_24/Tarea_2/ua2tarea2.java
+++ UA2/Tareas/dev_24/Tarea_2/ua2tarea2.java
@@ -5,98 +5,130 @@ import java.util.Set;
 import javax.annotation.processing.SupportedOptions;
 import javax.xml.stream.util.EventReaderDelegate;
 
-
 public class ua2tarea2 {
+    
     public static void main(String[] args) {
-        CuentaCorriente cuenta = new CuentaCorriente();
-        //creamos los hilos
-        Hilos hilo1 = new Hilos("Hilo1", 60,cuenta);
-        Hilos hilo2 = new Hilos("Hilo2", 3000,cuenta);
-        Hilos hilo3 = new Hilos("Hilo3", 43,cuenta);
-        Hilos hilo4 = new Hilos("Hilo4", 533,cuenta);
-        Hilos hilo5 = new Hilos("Hilo5", 122,cuenta);
-        //los iniciamos
+        
+        CuentaCorriente cuenta=new CuentaCorriente(30);
+       
+        Hilos hilo1=new Hilos("Hilo1", 60, cuenta);
+        Hilos hilo2=new Hilos("Hilo2", 200, cuenta);
+        Hilos hilo3=new Hilos("Hilo3", 300, cuenta);
+        Hilos hilo4=new Hilos("Hilo4", 56, cuenta);
+        Hilos hilo5=new Hilos("Hilo5", 100, cuenta);
+       
         hilo1.start();
         hilo2.start();
         hilo3.start();
         hilo4.start();
         hilo5.start();
-        //control de errores
-        try {
+        
+        try{
             hilo1.join();
             hilo2.join();
             hilo3.join();
             hilo4.join();
             hilo5.join();
-        } catch (Exception e) {
-            System.out.println("Error, pruebe de nuevo");
         }
+        catch(InterruptedException e){}
         
+        System.out.println("El saldo final es: "+cuenta.saldo+" €");
     }
 }
-//creamos la clase
-class CuentaCorriente {
-    float saldo;
-    float incremento;
-    public CuentaCorriente() {
-        this.saldo=0; 
+
+class CuentaCorriente{
+   
+    int saldo;
+    int incremento;
+   
+        this.saldo=0;
         this.incremento=0;
     }
   
+    public CuentaCorriente(int saldo){
+        this.saldo=saldo;
+    }
    
-    public void setSaldo(float saldo) {
+    public void setSaldo(int saldo) {
         this.saldo = saldo;
-        //usamos el sleep aleatorio
-        try {
-          Thread.sleep(int)(Math.random()*(1751+250));  
-        } catch (Exception e) {
-            
-        }
         
-     
+        try{
+        Thread.sleep((int)(Math.random()*(1751)+250));
+        }
+        catch(InterruptedException e){}
     }
-    public float getSaldo() {
-         return saldo;
-     }
-     public float getIncremento() {
-         return incremento;
-         
-     }
-     
-     public void setIncremento(float incremento) {
-         this.incremento = incremento;
-         
-     }
-
-    public synchronized void incrementar () {
+    public int getSaldo() {
+        try{
+        Thread.sleep((int)(Math.random()*(1751)+250));
+        }
+        catch(InterruptedException e){}
+        return saldo;
+    }
+    
+    public synchronized void incrementar(){
         incremento++;
+    }
+    
+    public synchronized void resultado(String nombre){
+        System.out.println("--------------------------------------");
+        System.out.println("El valor previo del saldo era: "+getSaldo()+"€");
+        System.out.println("El saldo actual es: "+(saldo+=incremento)+"€");
+       
+        incremento=0;
+        System.out.println("Quien ha realizado el ingreso es: "+nombre);
+        System.out.println("--------------------------------------");
+        System.out.println("");
+        System.out.println("");
+    }
+}
+
+class Hilos extends Thread{
+    
+    int num;
+    private CuentaCorriente cuenta;
+    
+    public Hilos(String nombre, int num, CuentaCorriente cuenta){
+        setName(nombre);
+        this.num=num;
+        this.cuenta=cuenta;
+    }
+   
+    public void run(){
         
-
-    }
-
-    public synchronized void resultado (String nombre) {
-        System.out.println("El saldo anteriormente era:"+saldo);
-        System.out.println("El saldo ahora es:" +(saldo+incremento));
-        System.out.println("La persona que ha realizado el ingreso es: "+nombre);   
-    }
-//creamos los hilos 
-    class Hilos extends Thread {
-        private CuentaCorriente cuenta;
-        private int numero;
+        for (int i=0; i<num; i++){
+            cuenta.incrementar();
+        }
         
-        public Hilos (String nombre, int numero, CuentaCorriente cuenta) {
-            
-            setName(nombre);
-            this.numero = numero;
-            this.cuenta =cuenta;
-        }
-//creamos el run para que realize el incremento
-        public synchronized void run(int numero) {
-            for (int j =0;j<numero;j++) {
-                cuenta.incrementar();
-            }
-            cuenta.resultado(getName());
-        }
+        cuenta.resultado(getName());
     }
-
+}
+public class ua2tarea2 {
+    
+    public static void main(String[] args) {
+        
+        CuentaCorriente cuenta=new CuentaCorriente(30);
+        
+        Hilos hilo1=new Hilos("Hilo1", 60, cuenta);
+        Hilos hilo2=new Hilos("Hilo2", 200, cuenta);
+        Hilos hilo3=new Hilos("Hilo3", 300, cuenta);
+        Hilos hilo4=new Hilos("Hilo4", 56, cuenta);
+        Hilos hilo5=new Hilos("Hilo5", 100, cuenta);
+       
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
+        hilo4.start();
+        hilo5.start();
+        
+        try{
+            hilo1.join();
+            hilo2.join();
+            hilo3.join();
+            hilo4.join();
+            hilo5.join();
+        }
+        catch(InterruptedException e){}
+        
+        System.out.println("El saldo final es: "+cuenta.saldo+" €");
+    }
 }
