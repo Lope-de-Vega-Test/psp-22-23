39948f07c26618711f3d76fdf52bca4dfa6b01f1
JavCast03 <101286156+JavCast03@users.noreply.github.com>
2022-12-21 13:51:11 +0100

    Create Ejecutar.java

---
+++ UA3/actividad/team10/Ejecutar.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class Ejecutar {
+    public static void main(String[] args) {       
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Introduzca el numero de miembrosToken: ");
+        int aux = entrada.nextInt();
+        String comando = "cmd.exe /c start "+"java MiembroToken.java";
+        for(int i = 2; i<=aux-1;i++){
+            try {
+                Process child = Runtime.getRuntime().exec(comando+" "+i+" "+(10000+i-1)+" no no");
+            } 
+            catch (IOException ex) {
+                Logger.getLogger(Ejecutar.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        try {
+                Process child = Runtime.getRuntime().exec(comando+" "+aux+" "+(10000+aux-1)+" no yes");
+            } 
+            catch (IOException ex) {
+                Logger.getLogger(Ejecutar.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        try {
+                Process child = Runtime.getRuntime().exec(comando+" "+1+" "+(10000)+" yes no");
+            } 
+            catch (IOException ex) {
+                Logger.getLogger(Ejecutar.class.getName()).log(Level.SEVERE, null, ex);
+            }
+}
+}
