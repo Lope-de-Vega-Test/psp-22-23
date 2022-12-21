749daae77cdd8617d74b8507367d1c155030da15
IgnacioJavier <100229246+IgnacioJavier@users.noreply.github.com>
2022-12-21 14:46:59 +0100

    Update MiembroToken.java

--- UA3/actividad/team_7/MiembroToken.java
+++ UA3/actividad/team_7/MiembroToken.java
@@ -16,7 +16,6 @@ public class MiembroToken {
     static boolean isFirst = false;
     static boolean isLast = false;
 
-
     /* @brief       Classic java main, starting execution
      * @param       arg[0] = [id]
      * @param       arg[1] = [puerto]
@@ -25,7 +24,6 @@ public class MiembroToken {
      */
     // java MiembroToken.java id puerto isFirst isLast
     public static void main(String[] args) throws InterruptedException {
-
         // CONTROL DE ERRORES: CANTIDAD DE PARAMENTROS Y FORMATO DE ENTRADA DE USUARIO
         if (args.length < 4 && args.length > 5) {
             System.out.println("Debes introducir al menos 4 parametros y maximo 5!");
@@ -73,299 +71,237 @@ public class MiembroToken {
                 }
             }
         } // FIN CONTROL ERRORES
-
         // CREACION Y EJECUCION DE UN HILO
-        if (args.length == 4) {
-            Hilo h = new Hilo(id, selfPort, isFirst, isLast);
-            h.start();
-            try {
-                h.join();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else if (args.length == 5) {
-            Hilo h = new Hilo(id, selfPort, isFirst, isLast, lastPort);
-            h.start();
-            try {
-                h.join();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        Hilo h = new Hilo(id, selfPort, isFirst, isLast, lastPort);
+        h.start();
+        try {
+            h.join();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
     } // FIN MAIN()
-
 } // FIN CLASE MIEMBROTOKEN
 
 class Hilo extends Thread {
 
     private int id;
-
     private int selfPort;
-    private int nextPort;
     private int lastPort;
-
     private boolean isFirst;
     private boolean isLast;
-
     private boolean hasToken;
-
     private Socket client;
     private ServerSocket server;
-
-    public Hilo(int id, int selfPort, boolean isFirst, boolean isLast) {
-        this.id = id;
-        this.isFirst = isFirst;
-        this.isLast = isLast;
-
-        this.selfPort = selfPort;
-
-        this.hasToken = false;
-
-        if (this.isFirst) {
-            this.hasToken = true;
-        }
-    }
 
     // CONSTRUCTOR PARA EL PRIMER MIEMBRO TOKEN, PARA QUE SEPA QUIEN ES EL ULTIMO PUERTO
     public Hilo(int id, int selfPort, boolean isFirst, boolean isLast, int lastPort) {
         this.id = id;
         this.isFirst = isFirst;
         this.isLast = isLast;
-
         this.selfPort = selfPort;
         this.lastPort = lastPort;
-
         this.hasToken = false;
-
         if (this.isFirst) {
             this.hasToken = true;
         }
     }
 
     public void run() {
-
         // ME PRESENTO
-        System.out.println("Miembro: " + id + ", Escucho Puerto: " + selfPort);
-
+        introducingMyself();
         // SIEMPRE ME CONFIGURO PARA ESCUCHAR!
-        try {
-            server = new ServerSocket(selfPort);
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (!serverSetUp()) {
+            System.out.println("Miembro: " + id + ", No pudo configurarse");
             System.exit(-1);
         }
-
         // QUIEN SOY?
         if (isFirst) { // SOY EL PRIMERO!
 
-            // EL PRIMERO SIEMPRE TIENE EL TOKEN
-            System.out.println("Miembro: " + id + ", Tiene el Token, es el Primero");
-            try {
-                Hilo.sleep(id * 1000);
-            } catch (Exception e) {
-            }
-
-            // INTENTO ENVIAR EL TOKEN
-            try {
-                nextPort = selfPort + 1;
-                client = new Socket("localhost", nextPort);
-                System.out.println("Miembro: " + id + ", Envio Token al puerto " + nextPort);
+            // INTENTO ENVIAR EL TOKEN HACIA DELANTE
+            // ESPERA TANTO TIEMPO COMO MIEMBROS HAYA (ESPERA A QUE A TODOS LOS MIEMBROS LES DE TIEMPO A CONFIGURARSE)
+            try{Hilo.sleep(Math.abs((lastPort-10000)*100));} catch (Exception e) {}
+            if (sendToken(selfPort + 1)) {
                 hasToken = false;
-                client.close();
-                System.out.println("Miembro: " + id + ", Esperando a que regrese el Token");
-            } catch (Exception e) {
+            } else {
                 System.out.println("Miembro: " + id + ", No pudo enviar el Token ):");
             }
 
-            // INTENTO ENVIAR EL TOKEN
-            try {
-                nextPort = lastPort;
-                client = new Socket("localhost", nextPort);
-                System.out.println("Miembro: " + id + ", Envio Token al puerto " + nextPort);
+            // INTENTO ENVIAR EL TOKEN HACIA ATRAS
+            // ESPERA TANTO TIMEPO COMO MIEMBROS HAYA (ESPERA A QUE A TODOS LOS MIEMBROS LES DE TIEMPO A CONFIGURARSE)
+            try{Hilo.sleep(Math.abs((lastPort-10000)*100));} catch (Exception e) {}
+            
+            if (sendToken(lastPort)) {
                 hasToken = false;
-                client.close();
-                System.out.println("Miembro: " + id + ", Esperando a que regrese el Token");
-            } catch (Exception e) {
+            } else {
                 System.out.println("Miembro: " + id + ", No pudo enviar el Token ):");
             }
 
-            // ESPERO A RECIBIR EL TOKEN
-            while (!hasToken) {
-                try {
-                    do {
-                        client = server.accept();
-                    } while (client == null);
-                    hasToken = true;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    //System.out.println("Estoy escuchando pero nadie me habla ):");
-                }
-            }
-            // COMPRUEBO SI RECIBI EL TOKEN
-            if (hasToken && client != null) {
-                System.out.println("Miembro: " + id + ", Token recibido");
-            }
+            // ESPERO A RECIBIR CUALQUIER TOKEN
+            do {
+                hasToken = waitForToken();
+            } while (!hasToken);
 
-            System.out.println("---");
-            System.out.println("Miembro: " + id + ", Recorrido hacia Delante finalizado");
-            System.out.println("---");
-
-            client = null;
             hasToken = false;
+            // ESPERO A RECIBIR EL OTRO TOKEN
+            do {
+                hasToken = waitForToken();
+            } while (!hasToken);
 
-            // ESPERO A RECIBIR EL TOKEN
-            while (!hasToken) {
-                try {
-                    do {
-                        client = server.accept();
-                    } while (client == null);
-                    hasToken = true;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    //System.out.println("Estoy escuchando pero nadie me habla ):");
-                }
-            }
-            // COMPRUEBO SI RECIBI EL TOKEN
-            if (hasToken && client != null) {
-                System.out.println("Miembro: " + id + ", Token recibido");
-            }
-
-            System.out.println("---");
-            System.out.println("Miembro: " + id + ", Recorrido hacia Atras finalizado");
             System.out.println("---");
             System.out.println("Miembro: " + id + ", Fin del programa");
-            System.out.println("---");
-
         } // FIN PRIMERO
         else if (isLast) { // SOY EL ULTIMO!
 
-            while (!hasToken) {
-                try {
-                    do {
-                        client = server.accept();
-                    } while (client == null);
-                    hasToken = true;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    //System.out.println("Estoy escuchando pero nadie me habla ):");
-                }
-            }
-
-            // RECIBO EL TOKEN Y ME DUERMO
-            System.out.println("Miembro: " + id + ", Token recibido");
-            try {
-                Hilo.sleep(id * 1000);
-            } catch (Exception e) {
-            }
+            // ESPERO A RECIBIR EL TOKEN DEL PRIMER MIEMBRO
+            do {
+                hasToken = waitForToken();
+            } while (!hasToken);
 
             // INTENTO ENVIAR EL TOKEN
-            try {
-                nextPort = selfPort - 1;
-                client = new Socket("localhost", nextPort);
-                System.out.println("Miembro: " + id + ", Envio Token al puerto " + nextPort);
+            if (sendToken(selfPort - 1)) {
                 hasToken = false;
-                client.close();
-            } catch (Exception e) {
+            } else {
                 System.out.println("Miembro: " + id + ", No pudo enviar el Token ):");
             }
 
-            while (!hasToken) {
-                try {
-                    do {
-                        client = server.accept();
-                    } while (client == null);
-                    hasToken = true;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    //System.out.println("Estoy escuchando pero nadie me habla ):");
-                }
-            }
-
-            // RECIBO EL TOKEN Y ME DUERMO
-            System.out.println("Miembro: " + id + ", Token recibido");
-            try {
-                Hilo.sleep(id * 1000);
-            } catch (Exception e) {
-            }
+            // ESPERO A RECIBIR EL OTRO TOKEN
+            do {
+                hasToken = waitForToken();
+            } while (!hasToken);
 
             // INTENTO ENVIAR EL TOKEN
-            try {
-                nextPort = 10000;
-                client = new Socket("localhost", nextPort);
-                System.out.println("Miembro: " + id + ", Envio Token al puerto " + nextPort);
+            if (sendToken(10000)) {
                 hasToken = false;
-                client.close();
-            } catch (Exception e) {
+            } else {
                 System.out.println("Miembro: " + id + ", No pudo enviar el Token ):");
             }
 
         } // FIN ULTIMO
         else { // NO SOY NI EL PRIMERO NI EL ULTIMO!
 
-            // SIEMPRE ME CONFIGURO PARA ESCUCHAR!
-            while (!hasToken) {
-                try {
-                    do {
-                        client = server.accept();
-                    } while (client == null);
-                    hasToken = true;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    //System.out.println("Estoy escuchando pero nadie me habla ):");
+            // ESPERO A RECIBIR CUALQUIER TOKEN
+            do {
+                hasToken = waitForToken();
+            } while (!hasToken);
+
+            // AVERIGUO SI ESTOY MAS CERCA DEL PRIMERO O DEL ULTIMO PARA EL PRIMER ENVIO DEL PRIMER TOKEN
+            int firstSentTo = 0;
+            if (getClosest() == -1) {
+                // INTENTO ENVIAR EL TOKEN HACIA DELANTE SI
+                if (sendToken(selfPort + 1)) {
+                    hasToken = false;
+                    firstSentTo = 1;
+                } else {
+                    System.out.println("Miembro: " + id + ", No pudo enviar el Token ):");
+                }
+            } else if (getClosest() == 1) {
+
+                // INTENTO ENVIAR EL TOKEN HACIA ATRAS
+                if (sendToken(selfPort - 1)) {
+                    hasToken = false;
+                    firstSentTo = -1;
+                } else {
+                    System.out.println("Miembro: " + id + ", No pudo enviar el Token ):");
                 }
             }
 
-            // RECIBO EL TOKEN Y ME DUERMO
-            System.out.println("Miembro: " + id + ", Token recibido");
-            try {
-                Hilo.sleep(id * 1000);
-            } catch (Exception e) {
-            }
+            do {
+                hasToken = waitForToken();
+            } while (!hasToken);
 
-            // INTENTO ENVIAR EL TOKEN
-            try {
-                nextPort = selfPort + 1;
-                client = new Socket("localhost", nextPort);
-                System.out.println("Miembro: " + id + ", Envio Token al puerto " + nextPort);
-                hasToken = false;
-                client.close();
-            } catch (Exception e) {
-                System.out.println("Miembro: " + id + ", No pudo enviar el Token ):");
-            }
-
-            while (!hasToken) {
-                try {
-                    do {
-                        client = server.accept();
-                    } while (client == null);
-                    hasToken = true;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    //System.out.println("Estoy escuchando pero nadie me habla ):");
+            // ENVIO EL SEGUNDO TOKEN HACIA DELANTE O HACIA ATRAS DEPENDIENDO DE A QUIEN LE ENVIE ANTES
+            if (firstSentTo == -1) {
+                if (sendToken(selfPort - 1)) {
+                    hasToken = false;
+                } else {
+                    System.out.println("Miembro: " + id + ", No pudo enviar el Token ):");
                 }
-            }
-
-            // RECIBO EL TOKEN Y ME DUERMO
-            System.out.println("Miembro: " + id + ", Token recibido");
-            try {
-                Hilo.sleep(id * 1000);
-            } catch (Exception e) {
-            }
-
-            // INTENTO ENVIAR EL TOKEN
-            try {
-                nextPort = selfPort - 1;
-                client = new Socket("localhost", nextPort);
-                System.out.println("Miembro: " + id + ", Envio Token al puerto " + nextPort);
-                hasToken = false;
-                client.close();
-            } catch (Exception e) {
-                System.out.println("Miembro: " + id + ", No pudo enviar el Token ):");
+            } else if (firstSentTo == 1) {
+                if (sendToken(selfPort + 1)) {
+                    hasToken = false;
+                } else {
+                    System.out.println("Miembro: " + id + ", No pudo enviar el Token ):");
+                }
+            } else {
+                if (sendToken(selfPort + 1)) {
+                    hasToken = false;
+                } else {
+                    System.out.println("Miembro: " + id + ", No pudo enviar el Token ):");
+                }
+                if (sendToken(selfPort - 1)) {
+                    hasToken = false;
+                } else {
+                    System.out.println("Miembro: " + id + ", No pudo enviar el Token ):");
+                }
             }
 
         } // FIN NI PRIMERO NI ULTIMO
-
     } // FIN RUN()
+
+    void introducingMyself() {
+        System.out.println("Miembro: " + id + ", Escucho Puerto: " + selfPort);
+        if (isFirst) {
+            System.out.println("Miembro: " + id + ", Tiene el Token, es el Primero");
+        }
+
+    }
+
+    boolean serverSetUp() {
+        boolean hasSetUp = false;
+        try {
+            Hilo.sleep(50);
+            server = new ServerSocket(selfPort);
+            hasSetUp = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return hasSetUp;
+    }
+
+    boolean sendToken(int toPort) {
+        boolean hasSentToken = false;
+        try {
+            client = new Socket("localhost", toPort);
+            System.out.println("Miembro: " + id + ", Envio Token al puerto " + toPort);
+            hasSentToken = true;
+            client.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return hasSentToken;
+    }
+
+    boolean waitForToken() {
+        boolean hasReceivedToken = false;
+
+        while (!hasReceivedToken) {
+            try {
+                do {
+                    client = server.accept();
+                } while (client == null);
+                hasReceivedToken = true;
+                System.out.println("Miembro: " + id + ", Token recibido");
+                Hilo.sleep(id * 1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return hasReceivedToken;
+    }
+
+    int getClosest() {
+        int closest = 0;
+        int diffToFirst = Math.abs(selfPort - 10000);
+        int diffToLast = Math.abs(selfPort - lastPort);
+
+        if (diffToFirst < diffToLast) {
+            closest = -1;
+        } else if (diffToLast < diffToFirst) {
+            closest = 1;
+        }
+
+        return closest;
+    }
 
 } // FIN CLASE HILO
