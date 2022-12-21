8cdcded7be15ebce47a8d436b0179d36e7e41425
JavCast03 <101286156+JavCast03@users.noreply.github.com>
2022-12-21 16:11:00 +0100

    Update MiembroToken.java

--- UA3/actividad/team10/MiembroToken.java
+++ UA3/actividad/team10/MiembroToken.java
@@ -1,12 +1,10 @@
 /**
  * @file        MiembroToken.java
- *
  * @brief       Solución para la actividad evaluable de la UA3.
  * @date        21/12/2022
  * @author      Rafael Damian Cristea Cristea Miembro 1 - github@email (dev_A)
  * @author      Luis Manuel Rodriguez Lopez Miembro 2 - github@email (dev_B)
  * @author      Javier Castilla Tejeda Miembro 3 - github@email (dev_C)
- *
  * @note        ¡Pero qué diablos!
  * @note        https://docs.google.com/presentation/d/e/2PACX-1vQzxbSl2IXLxelLggksAWEQd2tDT-5sNqETQCpIHCnNBqjoSmbvlJdCcOfu_rjQLz_BN6lsoOjimqSO/pub?start=false&loop=false&delayms=3000
  */
@@ -15,34 +13,34 @@ import java.net.*;
 public class MiembroToken{
     static class Hilos extends Thread{
         String[] args;
-        int puerto, puertoenvio, id;
+        int puerto, puertoAux, id;
         boolean token;
         Socket Envio, Escucha;
         ServerSocket Server;
         public Hilos(String args[]) throws IOException {
             this.puerto = Integer.parseInt(args[1]);;
             if (args[3].equals("no")){
-            this.puertoenvio = puerto+1;
+            this.puertoAux = puerto+1;
             }
-            else this.puertoenvio = 10000;
+            else this.puertoAux = 10000;
             this.id = Integer.parseInt(args[0]);
             this.token = args[2].equals("yes"); 
             this.Server = new ServerSocket(puerto);
         }
-        void EscucharToken() throws IOException{
+        void esperarT() throws IOException{
             System.out.println("Escuchando en " + puerto);
             Escucha = Server.accept();
             token = true;
         }
-        void MostrarToken() throws InterruptedException, IOException{
-            System.out.println("Esperando token");
+        void mostrarT() throws InterruptedException, IOException{
+            System.out.println("Esperando a que me den el token");
             Thread.sleep(id*1000);
-            EnviarToken();
+            enviarT();
             token = false;
         }
-        void EnviarToken() throws IOException{
-            System.out.println("Enviando a "+puertoenvio+"....");
-            Envio = new Socket("localhost", puertoenvio);
+        void enviarT() throws IOException{
+            System.out.println("Enviando a "+puertoAux+"....");
+            Envio = new Socket("localhost", puertoAux);
             Envio.close();
         }
         public void run()
@@ -50,17 +48,24 @@ public class MiembroToken{
             while(true)
             {
                 try{
-                    if(token){
-                        MostrarToken();
-                        System.out.println("No posee el token\n\n");
+                    if(token==true){
+                        System.out.println("-------------------------------");
+                        mostrarT();
+                        System.out.println("Ya no tengo el token");
+                        System.out.println("-------------------------------");
+                        System.out.println("");
                     }else{
-                        EscucharToken();
-                        System.out.println("Posee el token");
+                        System.out.println("-------------------------------");
+                        esperarT();
+                        System.out.println("Me han dado el token");
+                        System.out.println("-------------------------------");
+                        System.out.println("");
                     }
                 }catch(Exception e){
-                    System.out.println("No se puede ejecutar el run");
-                }
-                
+                    System.out.println("-------------------------------");
+                    System.out.println("No se encuentra token");
+                    System.out.println("-------------------------------");
+                } 
             }
         }
     }
@@ -68,5 +73,9 @@ public class MiembroToken{
         Hilos hilo = new Hilos(args);
         System.out.println("Id del hilo: "+hilo.id);
         hilo.start();
+        try{
+            hilo.join();
+        }
+        catch(Exception e){}
 }
 }
