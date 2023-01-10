d12c3a5f7ebb96ea5c899fa025d0910aec545a6a
JavCast03 <101286156+JavCast03@users.noreply.github.com>
2023-01-10 19:15:04 +0100

    Create ua3Servidor.java

---
+++ UA3/tareas/dev_9/tarea3/ua3Servidor.java
@@ -0,0 +1,19 @@
+import java.net.*;
+import java.io.*;
+public class ua3Servidor {
+    public static void main(String[] args) throws IOException {
+        ServerSocket socketServidor=new ServerSocket(100);
+        System.out.println("Escuchando en el puerto 100");
+        Socket Cliente=socketServidor.accept();
+        
+        BufferedReader recibido=new BufferedReader(new InputStreamReader(Cliente.getInputStream()));
+        String mensajeRecibido=recibido.readLine();
+        System.out.println("El mensaje es: "+mensajeRecibido);
+
+        PrintWriter enviar=new PrintWriter(Cliente.getOutputStream(), true);
+        String mensajeEnviado=mensajeRecibido.toUpperCase();
+        enviar.println(mensajeEnviado);
+        System.out.println("El mensaje ha sido mandado");
+        Cliente.close();
+    }
+}
