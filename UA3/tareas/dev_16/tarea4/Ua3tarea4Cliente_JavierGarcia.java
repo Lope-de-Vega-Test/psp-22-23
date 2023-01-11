42b2e8230e9f4a71131c2ac2198860fd7a9b7fab
JavierGarciaRuiz <99357715+JavierGarciaRuiz@users.noreply.github.com>
2023-01-11 12:51:47 +0100

    Add files via upload

---
+++ UA3/tareas/dev_16/tarea4/Ua3tarea4Cliente_JavierGarcia.java
@@ -0,0 +1,43 @@
+package ua3tarea4cliente_javiergarcia;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Ua3tarea4Cliente_JavierGarcia {
+
+    
+    public static void main(String[] args) throws IOException{
+        // TODO code application logic here
+        Scanner sca = new Scanner(System.in);
+        
+        Socket socket = new Socket("localhost", 5000);
+        
+        boolean finalizador = false;
+        String letra = "";
+        String mensajeEnviado = "";
+        String mensajeCliente = "";
+        
+        System.out.println("Introduce el mensaje:");
+        
+        while(!letra.equals("*")){
+            letra = sca.next();
+            if(!letra.equals("*")){
+                mensajeCliente += letra;
+            }
+        }
+
+        PrintWriter envio = new PrintWriter(socket.getOutputStream(), true);
+        envio.println(mensajeCliente);
+
+        System.out.println("Mensaje enviado");
+
+        BufferedReader recibo = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String mensajeServidor = recibo.readLine();
+
+        System.out.println("Mensaje de Servidor: " + mensajeServidor);
+
+        socket.close();
+    }
+    
+}
---
+++ UA3/tareas/dev_16/tarea4/Ua3tarea4Server_JavierGarcia.java
@@ -0,0 +1,30 @@
+package ua3tarea4server_javiergarcia;
+
+import java.io.*;
+import java.net.*;
+
+public class Ua3tarea4Server_JavierGarcia {
+
+    
+    public static void main(String[] args) throws IOException{
+        // TODO code application logic here
+        
+        ServerSocket socket = new ServerSocket(5000);
+
+        Socket socketCliente = socket.accept();
+
+        BufferedReader recibo = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+        String mensajeCliente = recibo.readLine();
+        
+        System.out.println("Mensaje del Cliente: " + mensajeCliente);
+
+        PrintWriter enviar = new PrintWriter(socketCliente.getOutputStream(), true);
+        String mensajeServidor = mensajeCliente.toUpperCase();
+        enviar.println(mensajeServidor);
+
+        System.out.println("Mensaje del Servidor: " + mensajeServidor);
+
+        socket.close();
+    }
+    
+}
