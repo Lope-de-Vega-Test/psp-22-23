4a40b5efff94e8a3c2ad8aa681f0e34e4da41810
luciagarciaaa <lucia44ee@gmail.com>
2023-01-11 18:28:15 +0100

    o

---
+++ UA3/tareas/dev_15/tarea4/tarea4_cliente.java
@@ -0,0 +1,42 @@
+//Cliente
+
+import java.io.*; 
+import java.net.*; 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class tarea4_cliente {
+
+    public static void main(String[] args) throws IOException {
+
+        DatagramSocket socket = new DatagramSocket();
+        InetAddress host = InetAddress.getLocalHost();
+        byte[] sendData;
+        byte[] receiveData = new byte[1024];
+
+        System.out.println("Introduce el mensaje o * para salir: ");
+        Scanner sc = new Scanner(System.in);
+        String message = sc.nextLine();
+
+        while(!message.equals("*")){
+            sendData = message.getBytes();
+            DatagramPacket packet = new DatagramPacket(sendData, sendData.length, host, 9876);
+            socket.send(packet);
+
+            packet = new DatagramPacket(receiveData, receiveData.length);
+            socket.receive(packet);
+            String respuesta = new String(packet.getData(), 0, packet.getLength());
+            System.out.println("Mensaje recibido del servidor: " + respuesta);
+
+            System.out.println("Introduce el mensaje o * para salir: ");
+            message = sc.nextLine();
+        }
+
+        sc.close();
+        socket.close();
+    }
+}
+
