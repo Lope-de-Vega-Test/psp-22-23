e20178a32589db2736499debe492e752d83a5395
Víctor Aljama Iglesias <95701225+victor-aljama@users.noreply.github.com>
2023-01-11 16:34:38 +0100

    Create ServidorChat.java

---
+++ UA3/examen/dev_3/ServidorChat.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.*;
+
+public class ServidorChat {
+    static final int MAXIMO = 5; // MAXIMO DE CONEXIONES PERMITIDAS
+
+    public static void main(String args[]) throws IOException {
+        int PUERTO = (44444); /* RELLENAR */
+
+        ServerSocket servidor = new ServerSocket(PUERTO);
+        System.out.println("Servidor iniciado...");
+
+        Socket tabla[] = new Socket[MAXIMO]; // para controlar las conexiones
+        ComunHilos comun = new ComunHilos(MAXIMO, 0, 0, tabla); /* RELLENAR */
+
+        while (comun.getCONEXIONES() < MAXIMO) {
+            Socket socket = new Socket(); // Se cierra en el HiloServidor
+            socket = servidor.accept();// esperando cliente /* RELLENAR */
+
+            comun.addTabla(socket, comun.getCONEXIONES());
+            comun.setACTUALES(comun.getACTUALES() + 1);
+            comun.setCONEXIONES(comun.getCONEXIONES() + 1);
+
+            HiloServidorChat hilo = new HiloServidorChat(socket, comun);
+            hilo.start(); /* RELLENAR */
+        }
+        servidor.close();
+
+    }// main
+}// ServidorChat..
