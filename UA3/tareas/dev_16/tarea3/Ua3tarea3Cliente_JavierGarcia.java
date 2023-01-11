1883556cbd56e71b2193ca25f8f059e79fbb472c
JavierGarciaRuiz <99357715+JavierGarciaRuiz@users.noreply.github.com>
2023-01-11 11:54:14 +0100

    Add files via upload

---
+++ UA3/tareas/dev_16/tarea3/Ua3tarea3Cliente_JavierGarcia.java
@@ -0,0 +1,36 @@
+package ua3tarea3cliente_javiergarcia;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Ua3tarea3Cliente_JavierGarcia {
+
+    
+    public static void main(String[] args) throws IOException{
+        // TODO code application logic here
+        Scanner sca = new Scanner(System.in);
+        
+        //Creamos el Socket con un puerto cualquiera teniendo en cuenta que en el programa del server 
+        //hay que poner el mismo puerto
+        Socket socket = new Socket("localhost", 5000);
+        
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        System.out.println("Escribe lo que desee:");
+        String mensajeCliente = sca.nextLine();//Mensaje que introduce el cliente
+        
+        out.println(mensajeCliente);
+        //Recibe la respuesta del servidor
+        String respuestaServer = in.readLine();
+        
+        //Se imprime la respuesta del servidor
+        System.out.println(respuestaServer);
+        
+        //Se cierra el socket
+        socket.close();
+    }
+    
+}
---
+++ UA3/tareas/dev_16/tarea3/Ua3tarea3Server_JavierGarcia.java
@@ -0,0 +1,31 @@
+package ua3tarea3server_javiergarcia;
+
+import java.io.*;
+import java.net.*;
+
+public class Ua3tarea3Server_JavierGarcia {
+
+    
+    public static void main(String[] args) throws IOException{
+        // TODO code application logic here
+        
+         //Creamos el Socket con un puerto cualquiera teniendo en cuenta que en el programa del cliente 
+        //hay que poner el mismo puerto
+        ServerSocket server = new ServerSocket(5000);
+        Socket socket = server.accept();
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        
+        //Recibe el mensaje del cliente
+        String mensajeCliente = in.readLine();
+        //La respuesta del server seria el mensaje del cliente en mayusculas
+        String respuestaServer = mensajeCliente.toUpperCase();
+        
+        //Imprimimos la respuesta del servidor
+        out.println(respuestaServer);
+        
+        //Se cierra el socket
+        socket.close();
+    }
+    
+}
