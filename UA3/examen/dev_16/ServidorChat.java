e1ae33db7290b61804649e19e298d9e9ded77829
JavierGarciaRuiz <99357715+JavierGarciaRuiz@users.noreply.github.com>
2023-01-11 16:55:25 +0100

    Add files via upload

---
+++ UA3/examen/dev_16/ServidorChat.java
@@ -0,0 +1,34 @@
+
+
+
+import java.io.*;
+import java.net.*;
+
+
+public class ServidorChat
+{
+	static final int MAXIMO = 5; //MAXIMO DE CONEXIONES PERMITIDAS
+
+	public static void main(String args[]) throws IOException {
+		int PUERTO = 44444;/* RELLENAR */
+
+		ServerSocket servidor = new ServerSocket(PUERTO);
+		System.out.println("Servidor iniciado...");
+
+		Socket tabla[] = new Socket[MAXIMO]; //para controlar las conexiones
+                ComunHilos comun = new ComunHilos(MAXIMO, 0, 0, tabla);
+
+		while (comun.getCONEXIONES() < MAXIMO) {
+			Socket socket = new Socket(); // Se cierra en el HiloServidor
+			socket = servidor.accept(); /* RELLENAR */ // esperando cliente
+
+			comun.addTabla(socket, comun.getCONEXIONES());
+			comun.setACTUALES(comun.getACTUALES() + 1);
+			comun.setCONEXIONES(comun.getCONEXIONES() + 1);
+
+			HiloServidorChat hilo = new HiloServidorChat(socket, comun);
+			hilo.start()/* RELLENAR */;
+		}
+		servidor.close();
+	}//main
+}//ServidorChat..
