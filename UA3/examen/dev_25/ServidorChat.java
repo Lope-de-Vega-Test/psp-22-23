65422539f3beb2a929422b379f093056cae0b6fd
RafaelRomeroDam <99832079+RafaelRomeroDam@users.noreply.github.com>
2023-01-11 16:48:08 +0100

    subida examen

---
+++ UA3/examen/dev_25/ServidorChat.java
@@ -0,0 +1,36 @@
+/*
+Rafael Romero Benítez
+ */
+import java.io.*;
+import java.net.*;
+/**
+ *
+ * @author RafaelRomero
+ */
+public class ServidorChat {
+static final int MAXIMO = 5; //MAXIMO DE CONEXIONES PERMITIDAS
+
+	public static void main(String args[]) throws IOException {
+		int PUERTO = 44444;
+
+		ServerSocket servidor = new ServerSocket(PUERTO);
+		System.out.println("Servidor iniciado...");
+
+		Socket tabla[] = new Socket[MAXIMO]; //para controlar las conexiones
+		ComunHilos comun = new ComunHilos (MAXIMO, 0, 0, tabla);
+
+		while (comun.getCONEXIONES() < MAXIMO) {
+			Socket socket = new Socket(); // Se cierra en el HiloServidor
+			socket = servidor.accept();
+
+			comun.addTabla(socket, comun.getCONEXIONES());
+			comun.setACTUALES(comun.getACTUALES() + 1);
+			comun.setCONEXIONES(comun.getCONEXIONES() + 1);
+
+			HiloServidorChat hilo = new HiloServidorChat(socket, comun);
+			hilo.start()/* RELLENAR */;
+		}
+		servidor.close();
+	}//main
+}//ServidorChat..
+
