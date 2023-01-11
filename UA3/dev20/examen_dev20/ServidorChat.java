3aa8f10a8c9b7095c1acad6a187a53610e586d8e
lucialunaposadas <114489539+lucialunaposadas@users.noreply.github.com>
2023-01-11 17:08:07 +0100

    Create ServidorChat.java

---
+++ UA3/dev20/examen_dev20/ServidorChat.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+/**
+ *
+ * @author Lucía Luna
+ */
+
+public class ServidorChat
+{
+	static final int MAXIMO = 5; //MAXIMO DE CONEXIONES PERMITIDAS
+
+	public static void main(String args[]) throws IOException {
+		int PUERTO = 4444; 
+
+		ServerSocket servidor = new ServerSocket(PUERTO);
+		System.out.println("Servidor iniciado...");
+
+		Socket tabla[] = new Socket[MAXIMO]; //para controlar las conexiones
+		ComunHilos comun = new ComunHilos(MAXIMO, 0, 0, tabla);
+
+		while (comun.getCONEXIONES() < MAXIMO) {
+			Socket socket = new Socket(); // Se cierra en el HiloServidor
+			Socket cliente = servidor.accept(); /* RELLENAR */  // esperando cliente
+
+			comun.addTabla(socket, comun.getCONEXIONES());
+			comun.setACTUALES(comun.getACTUALES() + 1);
+			comun.setCONEXIONES(comun.getCONEXIONES() + 1);
+
+			HiloServidorChat hilo = new HiloServidorChat(socket, comun);
+			hilo.close();/* RELLENAR */
+		}
+		servidor.close();
+	}//main
+}//ServidorChat..
