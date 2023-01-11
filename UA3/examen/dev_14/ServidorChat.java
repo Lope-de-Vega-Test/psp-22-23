2c02e1f1fc1cf8d26fcaf7bef3d0b348633f0046
DaniBalsera <99357708+DaniBalsera@users.noreply.github.com>
2023-01-11 17:16:55 +0100

    Add files via upload

---
+++ UA3/examen/dev_14/ServidorChat.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.net.*;
+
+public class ServidorChat {
+    static final int MAXIMO = 5;
+
+    public static void main(String args[]) throws IOException {
+        int PUERTO = 44444;
+        ServerSocket servidor = new ServerSocket(PUERTO);
+        System.out.println("Servidor iniciado...");
+
+
+        Socket tabla[] = new Socket[MAXIMO]; 
+        ComunHilos comun = new ComunHilos(MAXIMO, 0, 0, tabla);
+        while (comun.getCONEXIONES() < MAXIMO) {
+            Socket socket = new Socket(); 
+            socket = servidor.accept(); 
+            comun.addTabla(socket, comun.getCONEXIONES());
+            comun.setACTUALES(comun.getACTUALES() + 1);
+            comun.setCONEXIONES(comun.getCONEXIONES() + 1);
+            HiloServidorChat hilo = new HiloServidorChat(socket, comun);
+            hilo.start();
+            ;
+        }
+        servidor.close();
+    }// main
+}// ServidorChat..
+
+
+
+
+
