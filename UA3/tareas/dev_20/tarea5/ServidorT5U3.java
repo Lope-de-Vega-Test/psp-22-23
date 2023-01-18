73f49a19365160cf44e0a58ae301ad1428264215
lucialunaposadas <114489539+lucialunaposadas@users.noreply.github.com>
2023-01-18 16:55:29 +0100

    Create ServidorT5U3.java

---
+++ UA3/tareas/dev_20/tarea5/ServidorT5U3.java
@@ -0,0 +1,38 @@
+package servidort5u3;
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author Lucía Luna
+ */
+
+public class ServidorT5U3 {
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serversocket = new ServerSocket(6000);
+
+        System.out.println("Escuchando en el puerto 6000");
+
+        Socket socketcliente = serversocket.accept();
+
+        BufferedReader recibir = new BufferedReader(new InputStreamReader(socketcliente.getInputStream()));
+        int base = Integer.parseInt(recibir.readLine());
+        System.out.println("Numero recibido: " + base);
+
+        long cuadrado, cubo;
+       
+        cuadrado = base * base;
+        cubo = cuadrado * base;
+
+        PrintWriter enviar = new PrintWriter(socketcliente.getOutputStream(), true);
+        enviar.println(cuadrado + " " + cubo);
+
+        System.out.println("Cuadrado del numero: " + cuadrado);
+        System.out.println("Cubo del numero: " + cubo);
+
+        socketcliente.close();
+
+    }
+  
+}
