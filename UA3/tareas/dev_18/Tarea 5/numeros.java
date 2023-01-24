3a0e4852c35e948792b54b5df294a2179e76869c
Guillermo <56632130+guillejito@users.noreply.github.com>
2023-01-24 23:36:30 +0100

    Create numeros.java

---
+++ UA3/tareas/dev_18/Tarea 5/numeros.java
@@ -0,0 +1,66 @@
+public class numeros {
+    
+    int numerobase;
+    long cuadrado;
+    long cubo;
+
+    public Numeros(int numerobase, long cuadrado, long cubo) {
+      
+        this.numerobase = numerobase;
+        this.cuadrado = cuadrado;
+        this.cubo = cubo;
+      
+    }
+    
+    public bumeros() {
+      
+        numerobase = 0;
+        cuadrado  =0;
+        cubo = 0;
+      
+    }
+
+    public void setNumerobase(int numerobase) {
+      
+        this.numerobase = numerobase;
+      
+    }
+
+    public void setCuadrado(long cuadrado) {
+      
+        this.cuadrado = cuadrado;
+      
+    }
+
+    public void setCubo(long cubo) {
+      
+        this.cubo = cubo;
+      
+    }
+
+    public int getNumerobase() {
+      
+        return numerobase;
+      
+    }
+
+    public long getCuadrado() {
+      
+        return cuadrado;
+      
+    }
+
+    public long getCubo() {
+      
+        return cubo;
+      
+    }
+
+    @Override
+    public String toString() {
+      
+        return "numeros{"+"numeroBase="+numerobase+",cuadrado="+cuadrado+",cubo="+cubo+'}';
+      
+    }
+    
+}
