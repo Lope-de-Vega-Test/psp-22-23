af737a2a5492b9cb50bd3ff6c799fb76bcdb3c4a
Guillermo <56632130+guillejito@users.noreply.github.com>
2023-01-24 23:26:17 +0100

    Create Tarea4_server.java

---
+++ UA3/tareas/dev_18/Tarea4_server.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.*;
+
+public class Tarea4_server {
+
+	public static void main(String[] args) throws IOException {
+
+		ServerSocket server = new ServerSocket(2000);
+
+		Socket cliente = server.accept();
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+		String mensaje_Cliente = in.readLine();
+
+		System.out.println("Mensaje del Cliente: " + mensaje_Cliente);
+
+		PrintWriter enviar = new PrintWriter(cliente.getOutputStream(), true);
+		String respuestaServidor = mensaje_Cliente.toUpperCase();
+
+		enviar.println(respuestaServidor);
+
+		System.out.println("Respuesta del servidor: " + respuestaServidor);
+
+		server.close();
+    
+	}
+
+}
