240ee7c42aa69f320f569eaf24ff92d5f1b41c6b
lucialunaposadas <114489539+lucialunaposadas@users.noreply.github.com>
2023-01-25 10:49:41 +0100

    Update numerot5u3.java

--- UA3/tareas/dev_20/tarea5/numerot5u3.java
+++ UA3/tareas/dev_20/tarea5/numerot5u3.java
@@ -1,55 +1,55 @@
-package clientet5u3;
+package clienteej5;
 
 /**
  *
  * @author Lucía Luna
  */
-public class numerot5u3 {
-    int base;
-    long cuadrado,cubo;
+
+public class numerot5 {
+    int num;
+    long numcuadrado;
+    long numcubo;
+
+    public numerot5(int num, long numcuadrado, long numcubo) {
+        this.num = num;
+        this.numcuadrado = numcuadrado;
+        this.numcubo = numcubo;
+    }
     
-    
-       public numerot5u3() {
-           
-       }
-    
-        public numerot5u3(int base, long cuadrado, long cubo) {
-            this.base = base;
-            this.cuadrado = cuadrado;
-            this.cubo = cubo;
-        }
-        
-      //Creamos los setters
-    public void setBase(int base) {
-        this.base = base;
+    public numerot5() {
+        num = 0;
+        numcuadrado = 0;
+        numcubo = 0;
     }
 
-    public void setCuadrado(long cuadrado) {
-        this.cuadrado = cuadrado;
+    public int getNum() {
+        return num;
     }
 
-    public void setCubo(long cubo) {
-        this.cubo = cubo;
-    }
-        
-    
-    public int getBase() {
-        return base;
+    public void setNum(int num) {
+        this.num = num;
     }
 
-    public long getCuadrado() {
-        return cuadrado;
+    public long getNumcuadrado() {
+        return numcuadrado;
     }
 
-    //Creamos los getters
-    public long getCubo() {
-        return cubo;
+    public void setNumcuadrado(long numcuadrado) {
+        this.numcuadrado = numcuadrado;
+    }
+
+    public long getNumcubo() {
+        return numcubo;
+    }
+
+    public void setNumcubo(long numcubo) {
+        this.numcubo = numcubo;
     }
 
     @Override
     public String toString() {
-        return "numeros{" + "\n Base   " + base + "\n Cuadrado  " + cuadrado + "\n Cubo  " + cubo + '}';
+        return "Numeros [num=" + num + ", numcuadrado=" + numcuadrado + ", numcubo=" + numcubo + "]";
     }
-        
-       
+
+   
 }
