4dbd7844a03bb6d7492d3190d134ba228dd17a12
Adrián Luque <ladriank-education@gmail.com>
2023-02-01 15:20:47 +0100

    UA4 Assignment 2

---
+++ UA4/tareas/dev_21/tarea2/Person.java
@@ -0,0 +1,48 @@
+<<<<<<< HEAD
+public class Person {
+	private String name;
+	private String about;
+	private int birthYear;
+	
+	public Person(String name, String about, int birthYear) {
+		this.name = name;
+		this.about = about;
+		this.birthYear = birthYear;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAbout() {
+		return about;
+	}
+
+	public int getBirthYear() {
+		return birthYear;
+	}
+=======
+public class Person {
+	private String name;
+	private String about;
+	private int birthYear;
+	
+	public Person(String name, String about, int birthYear) {
+		this.name = name;
+		this.about = about;
+		this.birthYear = birthYear;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAbout() {
+		return about;
+	}
+
+	public int getBirthYear() {
+		return birthYear;
+	}
+>>>>>>> 66dadf1 (UA4 Assigment)
+}
