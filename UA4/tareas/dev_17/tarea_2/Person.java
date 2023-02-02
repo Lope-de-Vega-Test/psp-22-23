7c2fe7fce31269d1481e500c56e313c84a07a4cd
josepa10 <91134279+jopajapo10@users.noreply.github.com>
2023-02-02 04:27:28 +0100

    Add files via upload

---
+++ UA4/tareas/dev_17/tarea_2/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+	private String name;
+	private String about;
+	private int birthYear;
+	
+	public Person(String name, String about, int birthYear) {
+		this.name = name;
+		this.about = about;
+		this.birthYear = birthYear;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAbout() {
+		return about;
+	}
+
+	public int getBirthYear() {
+		return birthYear;
+	}
+}
