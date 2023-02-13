409a0b94ab87c7141aab419eab1d50856365c062
lucialunaposadas <114489539+lucialunaposadas@users.noreply.github.com>
2023-02-13 19:08:42 +0100

    Update Person.java

--- UA4/Tareas/dev_20/Tarea2/Person.java
+++ UA4/Tareas/dev_20/Tarea2/Person.java
@@ -2,7 +2,7 @@ public class Person {
 	private String name;
 	private String about;
 	private int birthYear;
-	
+
 	public Person(String name, String about, int birthYear) {
 		this.name = name;
 		this.about = about;
