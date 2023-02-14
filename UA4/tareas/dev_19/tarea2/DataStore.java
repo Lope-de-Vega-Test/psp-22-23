35fdb23bcdd03a09163e0538699772939b1e58ba
Álvaro Losada <99832565+alvarolosadaromero@users.noreply.github.com>
2023-02-14 17:12:49 +0100

    Update DataStore.java

--- UA4/tareas/dev_19/tarea2/DataStore.java
+++ UA4/tareas/dev_19/tarea2/DataStore.java
@@ -15,13 +15,19 @@ public class DataStore {
 		personMap.put("Ada", new Person("Ada", "Ada Lovelace was the first programmer.", 1815));
 		personMap.put("Kevin", new Person("Kevin", "Kevin is the author of HappyCoding.io.", 1986));
 		personMap.put("Stanley", new Person("Stanley", "Stanley is Kevin's cat.", 2007));
+		personMap.put("Paco", new Person("Paco", "Paco is NPC.", 1998));
 	}
 
-	public Person getPerson(String name) {
-		return personMap.get(name);
-	}
+    public Person getPerson(String name) {
+        return personMap.get(name);
+    }
 
-	public void putPerson(Person person) {
-		personMap.put(person.getName(), person);
-	}
+    public void putPerson(Person person) {
+        personMap.put(person.getName(), person);
+    }
+
+    public void deletePerson(String name)
+    {
+        personMap.remove(name);
+    }
 }
