5f5fa0770566f2be807789c017af81df2ed7f549
Rafael Damian Cristea <114081192+MrDamianCR@users.noreply.github.com>
2023-02-14 15:47:17 +0100

    Create Person.java

---
+++ UA4/tareas/dev_11/tarea2/Person.java
@@ -0,0 +1,30 @@
+public class Person {
+    private int id;
+    private String name;
+    private String about;
+    private int birthYear;
+
+    public Person(int id, String name, String about, int birthYear) {
+        this.id = id;
+        this.name = name;
+        this.about = about;
+        this.birthYear = birthYear;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+}
