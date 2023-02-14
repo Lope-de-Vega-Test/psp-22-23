975e01c3b94916644995586054cc5ce623bc4e34
Guillermo <56632130+guillejito@users.noreply.github.com>
2023-02-14 19:20:06 +0100

    Update PersonHandler.java

--- UA4/tareas/dev_18/Tarea4_UA4/PersonHandler.java
+++ UA4/tareas/dev_18/Tarea4_UA4/PersonHandler.java
@@ -10,36 +10,154 @@ class PersonHandler extends BasicHandler {
     }
 
     public void handle(HttpExchange exchange) throws IOException {
-        String remoteAddress = exchange.getRemoteAddress().getHostString();
-        System.out.println(
-                "[" + new Date() + "] Received GET " + exchange.getRequestURI() + " from client: " + remoteAddress);
+        try {
+            String remoteAddress = exchange.getRemoteAddress().getHostString();
+            System.out.println(
+                    "[" + new Date() + "] Received " + exchange.getRequestMethod() + " " + exchange.getRequestURI() +
+                            " from client: " + remoteAddress);
 
-        if ("GET".equals(exchange.getRequestMethod())) {
-            String responseString = "{";
+            if ("GET".equals(exchange.getRequestMethod())) {
+                String responseString = "{";
 
-            Map<String, String> params = queryToMap(exchange.getRequestURI().getQuery());
-            String personName = params.get("name");
-            Person person = store.getPerson(personName);
+                Map<String, String> params = queryToMap(exchange.getRequestURI().getQuery());
+                String personName = params.get("name");
+                Person person = store.getPerson(personName);
 
-            /*
-             * In the real world this part should be implemented this way:
-             * JSONObject jsonObject = new JSONObject();
-             * jsonObject.put("name", person.getName());
-             * jsonObject.put("about", person.getAbout());
-             * jsonObject.put("birthYear", person.getBirthYear());
-             * responseString = jsonObject.toString();
-             */
-            responseString += "\"name\": \"" + person.getName() + "\",";
-            responseString += "\"about\": \"" + person.getAbout() + "\",";
-            responseString += "\"birthYear\": " + person.getBirthYear() + "";
-            responseString += "}";
-            exchange.sendResponseHeaders(200, responseString.getBytes().length);
-            OutputStream output = exchange.getResponseBody();
-            output.write(responseString.getBytes());
-            output.flush();
-        } else {
-            exchange.sendResponseHeaders(405, -1); // 405 Method Not Allowed
+                /*
+                 * In the real world this part should be implemented this way:
+                 * JSONObject jsonObject = new JSONObject();
+                 * jsonObject.put("name", person.getName());
+                 * jsonObject.put("about", person.getAbout());
+                 * jsonObject.put("birthYear", person.getBirthYear());
+                 * responseString = jsonObject.toString();
+                 */
+
+                if (person == null) {
+                    responseString += "Info not found";
+                    responseString += "}";
+                    exchange.sendResponseHeaders(404, responseString.getBytes().length);
+                    OutputStream output = exchange.getResponseBody();
+                    output.write(responseString.getBytes());
+                    output.flush();
+                } else {
+                    responseString += "\"name\": \"" + person.getName() + "\",";
+                    responseString += "\"about\": \"" + person.getAbout() + "\",";
+                    responseString += "\"birthYear\": " + person.getBirthYear() + "";
+                    responseString += "}";
+                    exchange.sendResponseHeaders(200, responseString.getBytes().length);
+                    OutputStream output = exchange.getResponseBody();
+                    output.write(responseString.getBytes());
+                    output.flush();
+                }
+
+            } else if ("POST".equals(exchange.getRequestMethod())) {
+                String responseString = "{";
+                // 1. obtengo los datos de la persona de la URL
+                Map<String, String> params = queryToMap(exchange.getRequestURI().getQuery());
+                String personaNombre = params.get("name");
+                String personaInformacion = params.get("about");
+                int personaNacimiento = Integer.parseInt(params.get("birthYear"));
+                // 2. creo el objeto persona
+                Person persona = new Person(personaNombre, personaInformacion, personaNacimiento);
+                responseString += "\"name\": \"" + persona.getName() + "\",";
+                responseString += "\"about\": \"" + persona.getAbout() + "\",";
+                responseString += "\"birthYear\": " + persona.getBirthYear() + "";
+                responseString += "}";
+                if (persona.getName().equals("")) {
+                    throw new HttpException(400, "Bad Request");
+                } else {
+                    // 3. Guardo la nueva persona en el almacen
+                    store.putPerson(persona);
+                    // 4. Devuelvo 200 OK
+                    exchange.sendResponseHeaders(200, responseString.getBytes().length);
+                    OutputStream output1 = exchange.getResponseBody();
+                    output1.write(responseString.getBytes());
+                    output1.flush();
+                }
+            } else if ("PUT".equals(exchange.getRequestMethod())) {
+                String responseString = "{";
+                Map<String, String> params = queryToMap(exchange.getRequestURI().getQuery());
+                String personaNombre = params.get("name");
+                String personaInformacion = params.get("about");
+                int personaNacimiento = Integer.parseInt(params.get("birthYear"));
+
+                Person personaPUT = new Person(personaNombre, personaInformacion, personaNacimiento);
+                responseString += "\"name\": \"" + personaPUT.getName() + "\",";
+                responseString += "\"about\": \"" + personaPUT.getAbout() + "\",";
+                responseString += "\"birthYear\": " + personaPUT.getBirthYear() + "";
+                responseString += "}";
+                if (personaPUT.getName().equals("")) {
+                    throw new HttpException(400, "Bad Request");
+                } else {
+
+                    store.putPerson(personaPUT);
+
+                    exchange.sendResponseHeaders(200, responseString.getBytes().length);
+                    OutputStream output1 = exchange.getResponseBody();
+                    output1.write(responseString.getBytes());
+                    output1.flush();
+                }
+            } else if ("DELETE".equals(exchange.getRequestMethod())) {
+                String responseString = "";
+                Map<String, String> params = queryToMap(exchange.getRequestURI().getQuery());
+                String personaNombre = params.get("name");
+
+                // String personaInformacion = params.get("about");
+                // int personaNacimiento =Integer.parseInt(params.get("birthYear"));
+
+                // Person personaDELETE= new Person(personaNombre, personaInformacion,
+                // personaNacimiento);
+                // responseString += "\"name\": \"" + personaDELETE.getName() + "\",";
+                // responseString += "\"about\": \"" + personaDELETE.getAbout() + "\",";
+                // responseString += "\"birthYear\": " + personaDELETE.getBirthYear() + "";
+                // responseString += "}";
+
+                store.deletePerson(personaNombre);
+
+                // responseString += "\"name\": \"" + personaDELETE.getName() + "\",";
+                // responseString += "\"about\": \"" + personaDELETE.getAbout() + "\",";
+                // responseString += "\"birthYear\": " + personaDELETE.getBirthYear() + "";
+                // responseString += "}";
+
+                exchange.sendResponseHeaders(200, responseString.getBytes().length);
+                OutputStream output1 = exchange.getResponseBody();
+                output1.write(responseString.getBytes());
+                output1.flush();
+            } else {
+                throw new HttpException(405, "Method Not Allowed");
+            }
+        } catch (HttpException e) {
+            // si se produce una excepción de tipo HttpException, se envía la respuesta de
+            // error correspondiente
+            sendErrorResponse(exchange, e.getStatus(), e.getMessage());
+        } catch (Exception e) {
+            // si se produce una excepción no controlada, se envía una respuesta de error
+            // genérica
+            sendErrorResponse(exchange, 400, "Bad Request");
+            e.printStackTrace();
+        } finally {
+            exchange.close();
         }
-        exchange.close();
+    }
+
+    // envía una respuesta de error al cliente
+    private void sendErrorResponse(HttpExchange exchange, int status, String message) throws IOException {
+        exchange.sendResponseHeaders(status, message.getBytes().length);
+        OutputStream output = exchange.getResponseBody();
+        output.write(message.getBytes());
+        output.flush();
+    }
+}
+
+class HttpException extends Exception {
+    private final int status;
+
+    public HttpException(int status, String message) {
+        super(message);
+        this.status = status;
+    }
+
+    public int getStatus() {
+        return status;
     }
 }
