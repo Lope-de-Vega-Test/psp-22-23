1fb0ee72fb0c85f0dac5844455b2a6c124b467eb
JavierGarciaRuiz <99357715+JavierGarciaRuiz@users.noreply.github.com>
2023-02-15 16:02:37 +0100

    Add files via upload

---
+++ UA4/tareas/dev_16/tarea2/Person.java
@@ -0,0 +1,50 @@
+public class Person {
+	private int id;
+	private String name;
+	private String about;
+	private int birthYear;
+	
+	public Person(int id, String name, String about, int birthYear) {
+		this.id=id;
+		this.name = name;
+		this.about = about;
+		this.birthYear = birthYear;
+	}
+
+	public Person(){
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAbout() {
+		return about;
+	}
+
+	public int getBirthYear() {
+		return birthYear;
+	}
+
+	public void setId(int id){
+		this.id=id;
+	}
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setAbout(String about){
+        this.about=about;
+    }
+
+    public void setBirthYear(int birthYear){
+        this.birthYear=birthYear;
+    }
+
+}
