25cee968ae11fcc0f0e52fb3b8911348660fe589
Víctor Aljama Iglesias <95701225+victor-aljama@users.noreply.github.com>
2023-03-09 16:58:03 +0100

    Create Api.java

---
+++ UA4/tareas/dev_3/tarea_3/Api.java
@@ -0,0 +1,58 @@
+
+//package com.victoraljama;
+
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+
+
+public class Api {
+    
+    private String hostName;
+    private int port;
+    
+    public Api(){
+        this.hostName = "localhost";
+        this.port = 8080;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+    
+    public void crearApi() throws SocketException{
+        
+        gestionDatos data = new gestionDatos();
+        
+        HttpServer server = null;
+        try {
+            server = HttpServer.create(new InetSocketAddress(hostName, port), 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        server.createContext("/api/greeting", new GreetingHandler());
+        
+        server.createContext("/api/bye", new ByeHandler());
+        
+        server.createContext("/api/person", new PersonHandler(data.store));
+        
+        server.setExecutor(null); // creates a default executor
+        server.start();
+        System.out.println(
+                "El framework-less REST API servidor esta escuchando en " + server.getAddress().getAddress()
+                        + ":" + server.getAddress().getPort());
+    }
+}
