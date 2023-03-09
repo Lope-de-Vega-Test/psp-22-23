d9658452e28bdf459d42e38f6e0171324f7eaef2
Víctor Aljama Iglesias <95701225+victor-aljama@users.noreply.github.com>
2023-03-09 20:20:17 +0100

    Create KeyParGenerator.java

---
+++ UA5/tareas/tarea_2/KeyParGenerator.java
@@ -0,0 +1,43 @@
+
+//package com.mycompany.keypargenerator;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Gustavo Pacheco (ryctabo at gmail.com)
+ * @version 1.0-SNAPSHOT
+ */
+public class KeyParGenerator {
+
+    private static final Logger LOG = Logger.getLogger(KeyParGenerator.class.getName());
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        // Generating RSA Public and Private keys
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        PublicKey publicKey = keyPair.getPublic();
+        PrivateKey privateKey = keyPair.getPrivate();
+
+        // Saving RSA Public and Private keys
+        saveKey(publicKey, "id_bin_rsa.pub");
+        LOG.info(String.format("Public key format: %s", publicKey.getFormat()));
+
+        saveKey(privateKey, "id_bin_rsa.key");
+        LOG.info(String.format("Private key format: %s", privateKey.getFormat()));
+    }
+
+    private static void saveKey(Key key, String fileName) {
+        try (FileOutputStream out = new FileOutputStream(fileName)) {
+            out.write(key.getEncoded());
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+    }
+
+}
