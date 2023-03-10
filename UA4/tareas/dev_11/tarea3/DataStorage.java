4f3827fa68cfaaa16d25de14169c465bfa6912f3
Rafael Damian Cristea <114081192+MrDamianCR@users.noreply.github.com>
2023-03-10 20:00:50 +0100

    Create DataStorage.java

---
+++ UA4/tareas/dev_11/tarea3/DataStorage.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Example DataStore class that provides access to user data.
+ * Pretend this class accesses a database.
+ */
+public class DataStorage {
+
+    // Map of names to Person instances.
+    private Map<Integer, Person> personMap = new HashMap<>();
+
+    public DataStorage() {
+        // dummy data
+        personMap.put(1, new Person(1, "Ada", "Ada Lovelace was the first programmer.", 1815));
+        personMap.put(2, new Person(2, "Kevin", "Kevin is the author of HappyCoding.io.", 1986));
+        personMap.put(3, new Person(3, "Stanley", "Stanley is Kevin's cat.", 2007));
+    }
+
+    public Person getPerson(Integer id) {
+        //Usamos el metodo get, y haciendo uso de la variable name conseguimos la persona que queramos
+        return personMap.get(id);
+    }
+
+    public void putPerson(Person person) {
+        //Usando el metodo put, añadimos una persona con los datos de la variable tipo persona
+        personMap.put(person.getId(), person);
+    }
+
+    public void delPerson(Person person) {
+        //Haciendo uso del clear borramos la persona que indiquemos
+        personMap.clear();
+    }
+
+    public void actPerson(Person person, Person person2) {
+        //Con el metodo replace, intercambiamos "person", que es la persona que estamos apunto de actualizar
+        //por "person2" que es la persona con los datos nuevos
+        personMap.replace(person.getId(), person, person2);
+    }
+}
